7967097358ef0d3a8583e96f0b67710a1e511876
withal <withal@gmail.com>
2011-08-28 04:18:01 +0000

    Fixed typo.

--- mdp.py
+++ mdp.py
@@ -12,7 +12,7 @@ class MDP:
     """A Markov Decision Process, defined by an initial state, transition model,
     and reward function. We also keep track of a gamma value, for use by 
     algorithms. The transition model is represented somewhat differently from 
-    the text.  Instead of T(s, a, s') being  probability number for each 
+    the text.  Instead of T(s, a, s') being a probability number for each 
     state/action/state triplet, we instead have T(s, a) return a list of (p, s')
     pairs.  We also keep track of the possible states, terminal states, and 
     actions for each state. [page 615]"""
