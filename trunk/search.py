ae63c08e09107f867c9384365ee96c8fdb88e49a
withal@gmail.com <withal@gmail.com@2679dc44-f919-0410-802c-91c6f4a87680>
2011-09-02 01:53:02 +0000

    Corrected comment.

--- trunk/search.py
+++ trunk/search.py
@@ -190,7 +190,7 @@ def best_first_graph_search(problem, f):
     """Search the nodes with the lowest f scores first.
     You specify the function f(node) that you want to minimize; for example,
     if f is a heuristic estimate to the goal, then we have greedy best
-    first search; if f is node.depth then we have depth-first search.
+    first search; if f is node.depth then we have breadth-first search.
     There is a subtlety: the line "f = memoize(f, 'f')" means that the f
     values will be cached on the nodes as they are computed. So after doing
     a best first search you can examine the f values of the path returned."""
