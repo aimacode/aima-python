72bfc9d9f951746cc584b8e503110fa503e38043
withal <withal@gmail.com>
2011-09-15 01:22:17 +0000

    Fixed Wordlist bugs: hi must be int, and list may be empty.

--- search.py
+++ search.py
@@ -593,6 +593,7 @@ class Wordlist:
         words[i].startswith(prefix), or is None; the second is
         True iff prefix itself is in the Wordlist."""
         words = self.words
+        if hi is None: hi = len(words)
         i = bisect.bisect_left(words, prefix, lo, hi)
         if i < len(words) and words[i].startswith(prefix): 
             return i, (words[i] == prefix)
@@ -600,7 +601,7 @@ class Wordlist:
             return None, False
 
     def __contains__(self, word): 
-        return self.words[bisect.bisect_left(self.words, word)] == word
+        return self.lookup(word)[1]
 
     def __len__(self): 
         return len(self.words)
