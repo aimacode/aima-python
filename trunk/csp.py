aa8d2807690ce6db1314293b266559ea4d418730
withal@gmail.com <withal@gmail.com@2679dc44-f919-0410-802c-91c6f4a87680>
2011-10-03 23:02:35 +0000

    Added map of France. Fixed issue 11.

--- trunk/csp.py
+++ trunk/csp.py
@@ -352,6 +352,14 @@ usa = MapColoringCSP(list('RGBY'),
         NJ: DE; DE: MD; MD: DC; VT: NH MA; MA: NH RI CT; CT: RI; ME: NH;
         HI: ; AK: """)
 
+france = MapColoringCSP(list('RGBY'),
+        """AL: LO FC; AQ: MP LI PC; AU: LI CE BO RA LR MP; BO: CE IF CA FC RA
+        AU; BR: NB PL; CA: IF PI LO FC BO; CE: PL NB NH IF BO AU LI PC; FC: BO
+        CA LO AL RA; IF: NH PI CA BO CE; LI: PC CE AU MP AQ; LO: CA AL FC; LR:
+        MP AU RA PA; MP: AQ LI AU LR; NB: NH CE PL BR; NH: PI IF CE NB; NO:
+        PI; PA: LR RA; PC: PL CE LI AQ; PI: NH NO CA IF; PL: BR NB CE PC; RA:
+        AU BO FC PA LR""")
+
 #______________________________________________________________________________
 # n-Queens Problem
 
