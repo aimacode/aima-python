adcf24f3c39485b96a5d7826e67f56bc5c87ba28
withal@gmail.com <withal@gmail.com@2679dc44-f919-0410-802c-91c6f4a87680>
2011-10-03 23:01:00 +0000

    Fixes to USA map from issue 11.

--- trunk/csp.py
+++ trunk/csp.py
@@ -346,11 +346,12 @@ usa = MapColoringCSP(list('RGBY'),
         UT: WY CO AZ; MT: ND SD WY; WY: SD NE CO; CO: NE KA OK NM; NM: OK TX;
         ND: MN SD; SD: MN IA NE; NE: IA MO KA; KA: MO OK; OK: MO AR TX;
         TX: AR LA; MN: WI IA; IA: WI IL MO; MO: IL KY TN AR; AR: MS TN LA;
-        LA: MS; WI: MI IL; IL: IN; IN: KY; MS: TN AL; AL: TN GA FL; MI: OH;
+        LA: MS; WI: MI IL; IL: IN KY; IN: OH KY; MS: TN AL; AL: TN GA FL; MI: OH IN;
         OH: PA WV KY; KY: WV VA TN; TN: VA NC GA; GA: NC SC FL;
         PA: NY NJ DE MD WV; WV: MD VA; VA: MD DC NC; NC: SC; NY: VT MA CT NJ;
         NJ: DE; DE: MD; MD: DC; VT: NH MA; MA: NH RI CT; CT: RI; ME: NH;
         HI: ; AK: """)
+
 #______________________________________________________________________________
 # n-Queens Problem
 
