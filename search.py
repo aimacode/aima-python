4138da5cdadb5df336f4df033f1cb4408257b512
withal <withal@gmail.com>
2011-10-26 20:04:25 +0000

    In print_table() don't print trailing whitespace.

--- search.py
+++ search.py
@@ -789,14 +789,14 @@ def compare_searchers(problems, header, searchers=[breadth_first_tree_search,
 def compare_graph_searchers():
     """Prints a table of results like this:
 >>> compare_graph_searchers()
-Searcher                      Romania(A, B)        Romania(O, N)        Australia            
-breadth_first_tree_search     <  21/  22/  59/B>   <1158/1159/3288/N>   <   7/   8/  22/WA>  
-breadth_first_graph_search    <  11/  12/  28/B>   <  33/  34/  76/N>   <   6/   7/  19/WA>  
-depth_first_graph_search      <   9/  10/  23/B>   <  16/  17/  39/N>   <   4/   5/  13/WA>  
-iterative_deepening_search    <  11/  33/  31/B>   < 656/1815/1812/N>   <   3/  11/  11/WA>  
-depth_limited_search          <  54/  65/ 185/B>   < 387/1012/1125/N>   <  50/  54/ 200/WA>  
-astar_search                  <   3/   4/   9/B>   <   8/   9/  22/N>   <   2/   3/   6/WA>  
-recursive_best_first_search   < 200/ 201/ 601/B>   <  71/  72/ 213/N>   <  11/  12/  43/WA>  """
+Searcher                      Romania(A, B)        Romania(O, N)        Australia          
+breadth_first_tree_search     <  21/  22/  59/B>   <1158/1159/3288/N>   <   7/   8/  22/WA>
+breadth_first_graph_search    <  11/  12/  28/B>   <  33/  34/  76/N>   <   6/   7/  19/WA>
+depth_first_graph_search      <   9/  10/  23/B>   <  16/  17/  39/N>   <   4/   5/  13/WA>
+iterative_deepening_search    <  11/  33/  31/B>   < 656/1815/1812/N>   <   3/  11/  11/WA>
+depth_limited_search          <  54/  65/ 185/B>   < 387/1012/1125/N>   <  50/  54/ 200/WA>
+astar_search                  <   3/   4/   9/B>   <   8/   9/  22/N>   <   2/   3/   6/WA>
+recursive_best_first_search   < 200/ 201/ 601/B>   <  71/  72/ 213/N>   <  11/  12/  43/WA>"""
     compare_searchers(problems=[GraphProblem('A', 'B', romania),
                                 GraphProblem('O', 'N', romania),
                                 GraphProblem('Q', 'WA', australia)],
