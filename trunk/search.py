cb177d6499dd715e43af499ce538130aec38f0fd
withal@gmail.com <withal@gmail.com@2679dc44-f919-0410-802c-91c6f4a87680>
2011-10-28 23:45:37 +0000

    Don't compare strings with 'is not'. (Broke things in pypy.) Also show Struct members in sorted order so doctests work in pypy.

--- trunk/search.py
+++ trunk/search.py
@@ -204,7 +204,7 @@ def iterative_deepening_search(problem):
     "[Fig. 3.18]"
     for depth in xrange(sys.maxint):
         result = depth_limited_search(problem, depth)
-        if result is not 'cutoff':
+        if result != 'cutoff':
             return result
 
 #______________________________________________________________________________
