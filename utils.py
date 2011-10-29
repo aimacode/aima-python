06a94a2575a9537564f9196d03e7514d606ef4b6
withal <withal@gmail.com>
2011-10-29 03:34:21 +0000

    Fixed search bug reported by Matthew Yurka, and others:
     * graph_search missed deduping nodes in the frontier
     * astar_search didn't use 3rd-edition pseudocode
     * InstrumentedProblem didn't forward the path_cost method,
       so all the doctest results on instrumented cost-sensitive
       problems were wrong
    
    Also filled in breadth_first_search and per-node memoization 
    of h (to fulfill the doc comment saying that was done).
    
    There's a remaining problem that sometimes trivial refactorings
    change the amount of search that is done, slightly, which should
    not happen since the code is supposed to be deterministic.

--- utils.py
+++ utils.py
@@ -708,7 +708,8 @@ class FIFOQueue(Queue):
 class PriorityQueue(Queue):
     """A queue in which the minimum (or maximum) element (as determined by f and
     order) is returned first. If order is min, the item with minimum f(x) is
-    returned first; if order is max, then it is the item with maximum f(x)."""
+    returned first; if order is max, then it is the item with maximum f(x).
+    Also supports dict-like lookup."""
     def __init__(self, order=min, f=lambda x: x):
         update(self, A=[], order=order, f=f)
     def append(self, item):
@@ -722,6 +723,15 @@ class PriorityQueue(Queue):
             return self.A.pop()[1]
     def __contains__(self, item):
         return some(lambda (_, x): x == item, self.A)
+    def __getitem__(self, key):
+        for _, item in self.A:
+            if item == key:
+                return item
+    def __delitem__(self, key):
+        for i, (value, item) in enumerate(self.A):
+            if item == key:
+                self.A.pop(i)
+                return
 
 ## Fig: The idea is we can define things like Fig[3,10] later.
 ## Alas, it is Fig[3,10] not Fig[3.10], because that would be the same
