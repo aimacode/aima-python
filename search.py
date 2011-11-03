2cf81dcb2936b87a1c5b96b88d438ca20f3a8386
withal <withal@gmail.com>
2011-11-03 21:43:46 +0000

    Remove astar row from compare_graph_searchers table because it's sensitive to trivial changes. Needs real tests instead.

--- search.py
+++ search.py
@@ -805,7 +805,7 @@ def compare_searchers(problems, header,
                       searchers=[breadth_first_tree_search,
                                  breadth_first_search, depth_first_graph_search,
                                  iterative_deepening_search,
-                                 depth_limited_search, astar_search,
+                                 depth_limited_search,
                                  recursive_best_first_search]):
     def do(searcher, problem):
         p = InstrumentedProblem(problem)
@@ -823,7 +823,6 @@ breadth_first_search          <   7/  11/  18/B>   <  19/  20/  45/N>    <   2/ 
 depth_first_graph_search      <   8/   9/  20/B>   <  16/  17/  38/N>    <   4/   5/  11/WA>
 iterative_deepening_search    <  11/  33/  31/B>   < 656/1815/1812/N>    <   3/  11/  11/WA>
 depth_limited_search          <  54/  65/ 185/B>   < 387/1012/1125/N>    <  50/  54/ 200/WA>
-astar_search                  <   5/   7/  15/B>   <  16/  18/  40/N>    <   2/   4/   6/WA>
 recursive_best_first_search   <   5/   6/  15/B>   <5887/5888/16532/N>   <  11/  12/  43/WA>"""
     compare_searchers(problems=[GraphProblem('A', 'B', romania),
                                 GraphProblem('O', 'N', romania),
