523936c2c5c4ceae6f5c58027032e8f5985f4a15
norvig <peter@norvig.com>
2016-03-03 17:25:10 -0800

    Convert utils.py to Python 3

--- utils.py
+++ utils.py
@@ -1,279 +1,15 @@
-"""Provide some widely useful utilities. Safe for "from utils import *".
+"""Provide some widely useful functions and objects."""
 
-"""
+infinity = float('inf')
 
-from __future__ import generators
-import operator, math, random, copy, sys, os.path, bisect, re
+argmin = min
 
-assert (2,5) <= sys.version_info < (3,), """\
-This code is meant for Python 2.5 through 2.7.
-You might find that the parts you care about still work in older
-Pythons or happen to work in newer ones, but you're on your own --
-edit utils.py if you want to try it."""
+argmax = max
 
-#______________________________________________________________________________
-# Compatibility with Python 2.2, 2.3, and 2.4
+def ignore(x): None
 
-# The AIMA code was originally designed to run in Python 2.2 and up.
-# The first part of this file implements for Python 2.2 through 2.4
-# the parts of 2.5 that the original code relied on. Now we're
-# starting to go beyond what can be filled in this way, but here's
-# the compatibility code still since it doesn't hurt:
+def identity(x): return x
 
-try: bool, True, False ## Introduced in 2.3
-except NameError:
-    class bool(int):
-        "Simple implementation of Booleans, as in PEP 285"
-        def __init__(self, val): self.val = val
-        def __int__(self): return self.val
-        def __repr__(self): return ('False', 'True')[self.val]
-
-    True, False = bool(1), bool(0)
-
-try: sum ## Introduced in 2.3
-except NameError:
-    def sum(seq, start=0):
-        """Sum the elements of seq.
-        >>> sum([1, 2, 3])
-        6
-        """
-        return reduce(operator.add, seq, start)
-
-try: enumerate  ## Introduced in 2.3
-except NameError:
-    def enumerate(collection):
-        """Return an iterator that enumerates pairs of (i, c[i]). PEP 279.
-        >>> list(enumerate('abc'))
-        [(0, 'a'), (1, 'b'), (2, 'c')]
-        """
-        ## Copied from PEP 279
-        i = 0
-        it = iter(collection)
-        while 1:
-            yield (i, it.next())
-            i += 1
-
-
-try: reversed ## Introduced in 2.4
-except NameError:
-    def reversed(seq):
-        """Iterate over x in reverse order.
-        >>> list(reversed([1,2,3]))
-        [3, 2, 1]
-        """
-        if hasattr(seq, 'keys'):
-            raise TypeError("mappings do not support reverse iteration")
-        i = len(seq)
-        while i > 0:
-            i -= 1
-            yield seq[i]
-
-
-try: sorted ## Introduced in 2.4
-except NameError:
-    def sorted(seq, cmp=None, key=None, reverse=False):
-        """Copy seq and sort and return it.
-        >>> sorted([3, 1, 2])
-        [1, 2, 3]
-        """
-        seq2 = copy.copy(seq)
-        if key:
-            if cmp == None:
-                cmp = __builtins__.cmp
-            seq2.sort(lambda x,y: cmp(key(x), key(y)))
-        else:
-            if cmp == None:
-                seq2.sort()
-            else:
-                seq2.sort(cmp)
-        if reverse:
-            seq2.reverse()
-        return seq2
-
-try:
-    set, frozenset ## set builtin introduced in 2.4
-except NameError:
-    try:
-        import sets ## sets module introduced in 2.3
-        set, frozenset = sets.Set, sets.ImmutableSet
-    except (NameError, ImportError):
-        class BaseSet:
-            "set type (see http://docs.python.org/lib/types-set.html)"
-
-
-            def __init__(self, elements=[]):
-                self.dict = {}
-                for e in elements:
-                    self.dict[e] = 1
-
-            def __len__(self):
-                return len(self.dict)
-
-            def __iter__(self):
-                for e in self.dict:
-                    yield e
-
-            def __contains__(self, element):
-                return element in self.dict
-
-            def issubset(self, other):
-                for e in self.dict.keys():
-                    if e not in other:
-                        return False
-                return True
-
-            def issuperset(self, other):
-                for e in other:
-                    if e not in self:
-                        return False
-                return True
-
-
-            def union(self, other):
-                return type(self)(list(self) + list(other))
-
-            def intersection(self, other):
-                return type(self)([e for e in self.dict if e in other])
-
-            def difference(self, other):
-                return type(self)([e for e in self.dict if e not in other])
-
-            def symmetric_difference(self, other):
-                return type(self)([e for e in self.dict if e not in other] +
-                                  [e for e in other if e not in self.dict])
-
-            def copy(self):
-                return type(self)(self.dict)
-
-            def __repr__(self):
-                elements = ", ".join(map(str, self.dict))
-                return "%s([%s])" % (type(self).__name__, elements)
-
-            __le__ = issubset
-            __ge__ = issuperset
-            __or__ = union
-            __and__ = intersection
-            __sub__ = difference
-            __xor__ = symmetric_difference
-
-        class frozenset(BaseSet):
-            "A frozenset is a BaseSet that has a hash value and is immutable."
-
-            def __init__(self, elements=[]):
-                BaseSet.__init__(elements)
-                self.hash = 0
-                for e in self:
-                    self.hash |= hash(e)
-
-            def __hash__(self):
-                return self.hash
-
-        class set(BaseSet):
-            "A set is a BaseSet that does not have a hash, but is mutable."
-
-            def update(self, other):
-                for e in other:
-                    self.add(e)
-                return self
-
-            def intersection_update(self, other):
-                for e in self.dict.keys():
-                    if e not in other:
-                        self.remove(e)
-                return self
-
-            def difference_update(self, other):
-                for e in self.dict.keys():
-                    if e in other:
-                        self.remove(e)
-                return self
-
-            def symmetric_difference_update(self, other):
-                to_remove1 = [e for e in self.dict if e in other]
-                to_remove2 = [e for e in other if e in self.dict]
-                self.difference_update(to_remove1)
-                self.difference_update(to_remove2)
-                return self
-
-            def add(self, element):
-                self.dict[element] = 1
-
-            def remove(self, element):
-                del self.dict[element]
-
-            def discard(self, element):
-                if element in self.dict:
-                    del self.dict[element]
-
-            def pop(self):
-                key, val = self.dict.popitem()
-                return key
-
-            def clear(self):
-                self.dict.clear()
-
-            __ior__ = update
-            __iand__ = intersection_update
-            __isub__ = difference_update
-            __ixor__ = symmetric_difference_update
-
-
-
-
-#______________________________________________________________________________
-# Simple Data Structures: infinity, Dict, Struct
-
-infinity = 1.0e400
-
-def Dict(**entries):
-    """Create a dict out of the argument=value arguments.
-    >>> Dict(a=1, b=2, c=3)
-    {'a': 1, 'c': 3, 'b': 2}
-    """
-    return entries
-
-class DefaultDict(dict):
-    """Dictionary with a default value for unknown keys."""
-    def __init__(self, default):
-        self.default = default
-
-    def __getitem__(self, key):
-        if key in self: return self.get(key)
-        return self.setdefault(key, copy.deepcopy(self.default))
-
-    def __copy__(self):
-        copy = DefaultDict(self.default)
-        copy.update(self)
-        return copy
-
-class Struct:
-    """Create an instance with argument=value slots.
-    This is for making a lightweight object whose class doesn't matter."""
-    def __init__(self, **entries):
-        self.__dict__.update(entries)
-
-    def __cmp__(self, other):
-        if isinstance(other, Struct):
-            return cmp(self.__dict__, other.__dict__)
-        else:
-            return cmp(self.__dict__, other)
-
-    def __repr__(self):
-        args = ['%s=%s' % (k, repr(v)) for (k, v) in vars(self).items()]
-        return 'Struct(%s)' % ', '.join(sorted(args))
-
-def update(x, **entries):
-    """Update a dict; or an object with slots; according to entries.
-    >>> update({'a': 1}, a=10, b=20)
-    {'a': 10, 'b': 20}
-    >>> update(Struct(a=1), a=10, b=20)
-    Struct(a=10, b=20)
-    """
-    if isinstance(x, dict):
-        x.update(entries)
-    else:
-        x.__dict__.update(entries)
-    return x
 
 #______________________________________________________________________________
 # Functions on Sequences (mostly inspired by Common Lisp)
@@ -306,46 +42,6 @@ def product(numbers):
     """
     return reduce(operator.mul, numbers, 1)
 
-def count_if(predicate, seq):
-    """Count the number of elements of seq for which the predicate is true.
-    >>> count_if(callable, [42, None, max, min])
-    2
-    """
-    f = lambda count, x: count + (not not predicate(x))
-    return reduce(f, seq, 0)
-
-def find_if(predicate, seq):
-    """If there is an element of seq that satisfies predicate; return it.
-    >>> find_if(callable, [3, min, max])
-    <built-in function min>
-    >>> find_if(callable, [1, 2, 3])
-    """
-    for x in seq:
-        if predicate(x): return x
-    return None
-
-def every(predicate, seq):
-    """True if every element of seq satisfies predicate.
-    >>> every(callable, [min, max])
-    1
-    >>> every(callable, [min, 3])
-    0
-    """
-    for x in seq:
-        if not predicate(x): return False
-    return True
-
-def some(predicate, seq):
-    """If some element x of seq satisfies predicate(x), return predicate(x).
-    >>> some(callable, [min, 3])
-    1
-    >>> some(callable, [2, 3])
-    0
-    """
-    for x in seq:
-        px = predicate(x)
-        if px: return px
-    return False
 
 def isin(elt, seq):
     """Like (elt in seq), but compares with is, not ==.
@@ -358,25 +54,7 @@ def isin(elt, seq):
         if elt is x: return True
     return False
 
-#______________________________________________________________________________
-# Functions on sequences of numbers
-# NOTE: these take the sequence argument first, like min and max,
-# and like standard math notation: \sigma (i = 1..n) fn(i)
-# A lot of programing is finding the best value that satisfies some condition;
-# so there are three versions of argmin/argmax, depending on what you want to
-# do with ties: return the first one, return them all, or pick at random.
 
-def argmin(seq, fn):
-    """Return an element with lowest fn(seq[i]) score; tie goes to first one.
-    >>> argmin(['one', 'to', 'three'], len)
-    'to'
-    """
-    best = seq[0]; best_score = fn(best)
-    for x in seq:
-        x_score = fn(x)
-        if x_score < best_score:
-            best, best_score = x, x_score
-    return best
 
 def argmin_list(seq, fn):
     """Return a list of elements of seq[i] with the lowest fn(seq[i]) scores.
@@ -406,12 +84,7 @@ def argmin_random_tie(seq, fn):
                 best = x
     return best
 
-def argmax(seq, fn):
-    """Return an element with highest fn(seq[i]) score; tie goes to first one.
-    >>> argmax(['one', 'to', 'three'], len)
-    'three'
-    """
-    return argmin(seq, lambda x: -fn(x))
+
 
 def argmax_list(seq, fn):
     """Return a list of elements of seq[i] with the highest fn(seq[i]) scores.
@@ -588,18 +261,6 @@ def printf(format, *args):
     sys.stdout.write(str(format) % args)
     return if_(args, lambda: args[-1], lambda: format)
 
-def caller(n=1):
-    """Return the name of the calling function n levels up in the frame stack.
-    >>> caller(0)
-    'caller'
-    >>> def f():
-    ...     return caller()
-    >>> f()
-    'f'
-    """
-    import inspect
-    return inspect.getouterframes(inspect.currentframe())[n][3]
-
 def memoize(fn, slot=None):
     """Memoize fn: make it remember the computed value for any argument list.
     If slot is specified, store result in that slot of first argument.
@@ -620,30 +281,16 @@ def memoize(fn, slot=None):
         memoized_fn.cache = {}
     return memoized_fn
 
-def if_(test, result, alternative):
-    """Like C++ and Java's (test ? result : alternative), except
-    both result and alternative are always evaluated. However, if
-    either evaluates to a function, it is applied to the empty arglist,
-    so you can delay execution by putting it in a lambda.
-    >>> if_(2 + 2 == 4, 'ok', lambda: expensive_computation())
-    'ok'
-    """
-    if test:
-        if callable(result): return result()
-        return result
-    else:
-        if callable(alternative): return alternative()
-        return alternative
-
 def name(object):
     "Try to find some reasonable name for the object."
-    return (getattr(object, 'name', 0) or getattr(object, '__name__', 0)
-            or getattr(getattr(object, '__class__', 0), '__name__', 0)
-            or str(object))
+    return (getattr(object, 'name', False) or
+            getattr(object, '__name__', False) or
+            getattr(getattr(object, '__class__', None), '__name__', False) or
+            str(object))
 
 def isnumber(x):
-    "Is x a number? We say it is if it has a __int__ method."
-    return hasattr(x, '__int__')
+    "Is x a number? We say it is if it is a float, int, or complex."
+    return isinstance(x, (int, float, complex))
 
 def issequence(x):
     "Is x a sequence? We say it is if it has a __getitem__ method."
@@ -675,10 +322,6 @@ def AIMAFile(components, mode='r'):
 def DataFile(name, mode='r'):
     "Return a file in the AIMA /data directory."
     return AIMAFile(['..', 'data', name], mode)
-
-def unimplemented():
-    "Use this as a stub for not-yet-implemented functions."
-    raise NotImplementedError
 
 #______________________________________________________________________________
 # Queues: Stack, FIFOQueue, PriorityQueue
@@ -763,7 +406,7 @@ Fig = {}
 #______________________________________________________________________________
 # Support for doctest
 
-def ignore(x): None
+
 
 def random_tests(text):
     """Some functions are stochastic. We want to be able to write a test
