e046bcc7edce9084302d1498c012586a9aa71cc8
norvig <peter@norvig.com>
2016-03-04 18:09:54 -0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
 """Provide some widely useful utilities. Safe for "from utils import *".
+
+TODO: Let's take the >>> doctest examples out of the docstrings, and put them in utils_test.py
+TODO: count_if and the like are leftovers from COmmon Lisp; let's make replace thenm with Pythonic alternatives.
+TODO: if_ is a terrible idea; replace all uses with (x if test else y) and remove if_
+TODO: Create a separate grid.py file for 2D grid environments; move headings, etc there.
+TODO: Priority queues may not belong here -- see treatment in search.py
 """
 
 import operator
