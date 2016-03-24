c39f09683c8d36a26203c22fee84e552f9dbbfad
root <root@localhost.localdomain>
2016-03-25 04:03:28 +0530

    len() in genetic_algorithm optimised

--- search.py
+++ search.py
@@ -477,7 +477,8 @@ def genetic_algorithm(population, fitness_fn, ngen=1000, pmut=0.1):
     "[Fig. 4.8]"
     for i in range(ngen):
         new_population = []
-        for i in len(population):
+        len_population = len(population)
+        for i in len_population:
             fitnesses = list(map(fitness_fn, population))
             p1, p2 = weighted_sample_with_replacement(population, fitnesses, 2)
             child = p1.mate(p2)
