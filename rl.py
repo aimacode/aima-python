420cf62ac02a81fe9f14bdb7e1e342a913a50c7d
Tarun Kumar <reachtarunhere@gmail.com>
2016-03-24 20:53:50 +0530

    Implemented Function to Graph Utility vs Iteration Plots

--- rl.py
+++ rl.py
@@ -3,6 +3,7 @@
 
 from utils import *  # noqa
 import agents
+import pylab
 
 
 class PassiveADPAgent(agents.Agent):
@@ -83,3 +84,20 @@ def run_single_trial(agent_program, mdp):
         if next_action is None:
             break
         current_state = take_single_action(mdp, current_state, next_action)
+
+
+def graph_utility_estimates(agent_program, mdp, no_of_iterations, states_to_graph):
+    ''' Generates matplotlib plots for utility of particular state
+    vs iterations passed. Simiar to [Fig. 21.3a] or [Fig. 21.5a]'''
+
+    graphs = {state: [] for state in states_to_graph}
+    for iteration in range(1, no_of_iterations+1):
+        run_single_trial(agent_program, mdp)
+        for state in states_to_graph:
+            graphs[state].append((iteration, agent_program.U[state]))
+    for state, value in graphs.items():
+        state_x, state_y = zip(*value)
+        pylab.plt.plot(state_x, state_y, label=str(state))
+    pylab.ylim([0, 1.2])
+    pylab.legend(loc='lower right')
+    pylab.plt.show()
