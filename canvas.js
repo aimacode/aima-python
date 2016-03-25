57352fb62dbddb4b010e827845797c3f83c2a863
Chipe1 <cg_vedant@yahoo.com>
2016-03-25 15:46:18 +0530

    Added mouseclick to canvas

--- canvas.js
+++ canvas.js
@@ -1,10 +1,20 @@
-function click_callback(element, event){
+function output_callback(out, block){
+    console.log(out);
+    script = out.content.data['text/html'];
+    console.log(script);
+    script = script.substr(8, script.length - 17);
+    console.log(script);
+    eval(script)
+}
+
+function click_callback(element, event, varname){
     var rect = element.getBoundingClientRect();
     var x = event.clientX - rect.left;
     var y = event.clientY - rect.top;
     var kernel = IPython.notebook.kernel;
-    var exec_str = "a.click("+String(x)+", "+String(y)+")";
-    kernel.execute(exec_str);
+    var exec_str = varname + ".mouse_click(" + String(x) + ", " + String(y) + ")";
+    console.log(exec_str);
+    kernel.execute(exec_str,{'iopub': {'output': output_callback}}, {silent: false});
 }
 
 function rgbToHex(r,g,b){
@@ -21,21 +31,26 @@ function toRad(x){
 
 //Canvas class to store variables
 function Canvas(id){
-    this.canvas=document.getElementById(id);
-    this.ctx=this.canvas.getContext("2d");
-    this.WIDTH=this.canvas.width;
-    this.HEIGHT=this.canvas.height;
-    this.MOUSE={x:0,y:0};
+    this.canvas = document.getElementById(id);
+    this.ctx = this.canvas.getContext("2d");
+    this.WIDTH = this.canvas.width;
+    this.HEIGHT = this.canvas.height;
+    this.MOUSE = {x:0,y:0};
 }
 
 Canvas.prototype.fill = function(r, g, b){
     //Sets the fill color
-    this.ctx.fillStyle=rgbToHex(r,g,b);
+    this.ctx.fillStyle = rgbToHex(r,g,b);
 }
 
 Canvas.prototype.stroke = function(r, g, b){
     //Set the stroke color
-    this.ctx.strokeStyle=rgbToHex(r,g,b);
+    this.ctx.strokeStyle = rgbToHex(r,g,b);
+}
+
+Canvas.prototype.strokeWidth = function(w){
+    //Set width of the line
+    this.ctx.lineWidth = w;
 }
 
 Canvas.prototype.rect = function(x, y, w, h){
@@ -43,19 +58,20 @@ Canvas.prototype.rect = function(x, y, w, h){
 }
 
 Canvas.prototype.line = function(x1, y1, x2, y2){
-    this.ctx.moveTo(x1,y1);
-    this.ctx.lineTo(x2,y2);
+    this.ctx.beginPath();
+    this.ctx.moveTo(x1, y1);
+    this.ctx.lineTo(x2, y2);
     this.ctx.stroke();
 }
 
 Canvas.prototype.arc = function(x, y, r, start, stop){
     this.ctx.beginPath();
-    this.ctx.arc(x,y,r,toRad(start),toRad(stop));
+    this.ctx.arc(x, y, r, toRad(start), toRad(stop));
     this.ctx.stroke();
 }
 
 Canvas.prototype.clear = function(){
-    this.ctx.clearRect(0,0,this.WIDTH,this.HEIGHT);
+    this.ctx.clearRect(0, 0, this.WIDTH, this.HEIGHT);
 }
 
 Canvas.prototype.test_run = function(){
@@ -65,22 +81,22 @@ Canvas.prototype.test_run = function(){
     this.clear();
     if(dbg)
 	alert("2");
-    this.fill(0,200,0);
+    this.fill(0, 200, 0);
     if(dbg)
 	alert("3");
-    this.rect(this.MOUSE.x,this.MOUSE.y,100,200);
+    this.rect(this.MOUSE.x, this.MOUSE.y, 100, 200);
     if(dbg)
 	alert("4");
-    this.stroke(0,0,50);
+    this.stroke(0, 0, 50);
     if(dbg)
 	alert("5");
-    this.line(0,0,100,100);
+    this.line(0, 0, 100, 100);
     if(dbg)
 	alert("6");
-    this.stroke(200,200,200);
+    this.stroke(200, 200, 200);
     if(dbg)
 	alert("7");
-    this.arc(200,100,50,0,360);
+    this.arc(200, 100, 50, 0, 360);
     if(dbg)
 	alert("8");
 }
--- canvas.py
+++ canvas.py
@@ -3,7 +3,7 @@ from IPython.display import HTML, display, clear_output
 _canvas = """
 <script type="text/javascript" src="./canvas.js" />
 <div>
-<canvas id="{0}" width="{1}" height="{2}" style="background:rgba(158, 167, 184, 0.2);" onclick="click_callback(this, event)"/>
+<canvas id="{0}" width="{1}" height="{2}" style="background:rgba(158, 167, 184, 0.2);" onclick='click_callback(this, event, "{3}")'/>
 </div>
 
 <script> var {0}_canvas_object = new Canvas("{0}");</script>
@@ -33,14 +33,23 @@ class Canvas:
     def stroke(self, r, g, b):
         self.exec_list.append("{0}_canvas_object.stroke({1}, {2}, {3});".format(self.id, r, g, b))
 
+    def strokeWidth(self, w):
+        self.exec_list.append("{0}_canvas_object.strokeWidth({1});".format(self.id, w))
+
     def rect(self, x, y, w, h):
-        self.exec_list.append("{0}_canvas_object.rect({1}, {2}, {3}, {4});".format(self.id, x, y, w, h))   
+        self.exec_list.append("{0}_canvas_object.rect({1}, {2}, {3}, {4});".format(self.id, x*self.width, y*self.height, w*self.width, h*self.height))   
 
     def line(self, x1, y1, x2, y2):
-        self.exec_list.append("{0}_canvas_object.line({1}, {2}, {3}, {4});".format(self.id, x1, y1, x2, y2))
+        self.exec_list.append("{0}_canvas_object.line({1}, {2}, {3}, {4});".format(self.id, x1*self.width, y1*self.height, x2*self.width, y2*self.height))
 
     def arc(self, x, y, r, start, stop):
-        self.exec_list.append("{0}_canvas_object.arc({1}, {2}, {3}, {4}, {5});".format(self.id, x, y, r, start, stop))
+        self.exec_list.append("{0}_canvas_object.arc({1}, {2}, {3}, {4}, {5});".format(self.id, x*self.width, y*self.height, r*min(self.width, self.height), start, stop))
+
+    def clear(self):
+        self.exec_list.append("{0}_canvas_object.clear();".format(self.id))
+
+    def alert(self, message):
+        display(HTML('<script>alert("{0}")</script>'.format(message)))
 
     def update(self):        
         exec_code = "<script>\n"+'\n'.join(self.exec_list)+"\n</script>"
