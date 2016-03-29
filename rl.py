9ebf2047f93cc31d1aec88a3dfc02d9da94d6674
Tarun Kumar <reachtarunhere@gmail.com>
2016-03-30 01:07:40 +0530

    Added sane default for update_state

--- rl.py
+++ rl.py
@@ -51,7 +51,9 @@ class PassiveTDAgent:
         return self.a
 
     def update_state(self, percept):
-        raise NotImplementedError
+        ''' To be overriden in most cases. The default case
+        assumes th percept to be of type (state, reward)'''
+        return percept
 
 
 def run_single_trial(agent_program, mdp):
