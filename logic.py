ed417956d1f6c49a12b704363071a9e3dd78a1bd
C.G.Vedant <cgvedant@gmail.com>
2016-04-09 04:52:17 +0530

    Implemented SAT_plan (#198)
    * Fixed a typo in logic.py
    
    * Added translate_to_SAT()
    
    * extract solution from model
    
    * added test cases
    
    * removed debug code

--- logic.py
+++ logic.py
@@ -678,7 +678,79 @@ def plan_route(current, goals, allowed):
 
 
 def SAT_plan(init, transition, goal, t_max, SAT_solver=dpll_satisfiable):
-    "[Fig. 7.22]"
+    """Converts a planning problem to Satisfaction problem by translating it to a cnf sentence.
+    TODO : Currently it fails if '_' character is used in transition. Change the expression names
+    [Fig. 7.22]"""
+
+    #Functions used by SAT_plan
+    def translate_to_SAT(init, transition, goal, time):
+        action_sym = "State_{0}_{1}"
+        clauses = []
+        states = [state for state in transition]
+        
+        #Symbol claiming state s at time t
+        state_sym = {}
+        for s in states:
+            for t in range(time+1):
+                state_sym[(s, t)] = Expr("In_{0}_at_{1}".format(s, t))
+
+        #Add initial state axiom
+        clauses.append(state_sym[init, 0])
+
+        #Add goal state axiom
+        clauses.append(state_sym[goal, time])
+
+        #All possible transitions
+        action_sym = {}
+        for s in states:
+            for action in transition[s]:
+                s_ = transition[s][action]
+                for t in range(time):
+                    #Action 'action' taken from state 's' at time 't' to reach 's_'
+                    action_sym[(s, action, t)] = Expr("Act_{0}_{1}_{2}".format(s, action, t))
+
+                    # Change the state from s to s_
+                    clauses.append(action_sym[s, action, t] >> state_sym[s, t])
+                    clauses.append(action_sym[s, action, t] >> state_sym[s_, t + 1])
+
+        #Allow only one state at any time
+        for t in range(time+1):
+            #must be a state at any time
+            clauses.append(associate('|', [ state_sym[s, t] for s in states ]))
+
+            for s in states:
+                for s_ in states[states.index(s)+1:]:
+                    #for each pair of states s, s_ only one is possible at time t
+                    clauses.append((~state_sym[s, t]) | (~state_sym[s_, t]))
+
+        #Restrict to one transition per timestep
+        for t in range(time):
+            #list of possible transitions at time t
+            transitions_t = [tr for tr in action_sym if tr[2] == t]
+
+            #make sure atleast one of the transition happens
+            clauses.append(associate('|', [ action_sym[tr] for tr in transitions_t ]))
+
+            for tr in transitions_t:
+                for tr_ in transitions_t[transitions_t.index(tr) + 1 :]:
+                    #there cannot be two transitions tr and tr_ at time t
+                    clauses.append((~action_sym[tr]) | (~action_sym[tr_]))
+
+        #Combine the clauses to form the cnf
+        return associate('&', clauses)
+
+    def extract_solution(model):
+        true_syms = [ sym.__repr__() for sym in model if model[sym] ]
+        true_transitions = [ sym for sym in true_syms if sym.startswith('Act_')]
+        solution = []
+        for sym in true_transitions:
+            # Extract time and action from 'Act_state_action_time'
+            solution.append((int(sym.split('_')[-1]), sym.split('_')[2]))
+        #Sort the actions based on time
+        solution.sort()
+        return [ action for time,action in solution ]
+
+    #Body of SAT_plan algorithm
     for t in range(t_max):
         cnf = translate_to_SAT(init, transition, goal, t)
         model = SAT_solver(cnf)
@@ -686,13 +758,6 @@ def SAT_plan(init, transition, goal, t_max, SAT_solver=dpll_satisfiable):
             return extract_solution(model)
     return None
 
-
-def translate_to_SAT(init, transition, goal, t):
-    unimplemented()
-
-
-def extract_solution(model):
-    unimplemented()
 
 # ______________________________________________________________________________
 
@@ -776,12 +841,7 @@ def subst(s, x):
 
 
 def fol_fc_ask(KB, alpha):
-    """Inefficient forward chaining for first-order logic. [Fig. 9.3]
-    KB is a FolKB and alpha must be an atomic sentence."""
-    while True:
-        for r in KB.clauses:
-            ps, q = parse_definite_clause(standardize_variables(r))
-            raise NotImplementedError
+    unimplemented()
 
 
 def standardize_variables(sentence, dic=None):
@@ -872,7 +932,7 @@ crime_kb = FolKB(
 
 def fol_bc_ask(KB, query):
     """A simple backward-chaining algorithm for first-order logic. [Fig. 9.6]
-    KB should be an instance of FolKB, and goals a list of literals. """
+    KB should be an instance of FolKB, and query an atomic sentence. """
     return fol_bc_or(KB, query, {})
 
 
