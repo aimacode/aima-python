1a656d5e2143980a4b9d3aa1522a40b8466ce5d8
Tarun Kumar <reachtarunhere@gmail.com>
2016-04-09 12:33:34 +0530

    Made Log Usage Consistent in text.py

--- text.py
+++ text.py
@@ -154,8 +154,8 @@ class IRSystem:
     def score(self, word, docid):
         "Compute a score for this word on the document with this docid."
         # There are many options; here we take a very simple approach
-        return (math.log(1 + self.index[word][docid]) /
-                math.log(1 + self.documents[docid].nwords))
+        return (log(1 + self.index[word][docid]) /
+                log(1 + self.documents[docid].nwords))
 
     def total_score(self, words, docid):
         "Compute the sum of the scores of these words on the document with this docid."
