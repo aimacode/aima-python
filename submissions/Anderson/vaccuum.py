90a2d2d32f2f2be0ed7c7723c580a3bf0e399ef6
zayneanderson <zayne.anderson@pop.belmont.edu>
2016-09-05 16:54:33 -0500

    Homework #1 finished added pinkguy background

--- submissions/Anderson/vaccuum.py
+++ submissions/Anderson/vaccuum.py
@@ -198,7 +198,7 @@ v.scatter_things(Dirt)
 g = gui.EnvGUI(v, 'Vaccuum')
 c = g.getCanvas()
 c.mapImageNames({
-    ag.Wall: 'images/pinkguy.jpg',
+    ag.Wall: 'submissions/Anderson/pinkguy.jpg',
     # Floor: 'images/floor.png',
     Dirt: 'images/dirt.png',
     ag.Agent: 'images/vacuum.png',
