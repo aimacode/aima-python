a10096447d0f8a085ea328959c22bbfbab59937d
austinban <banaustinban@gmail.com>
2016-09-06 14:49:33 -0500

    Got it!
    It’s all working, not super efficiently, but it would cover any size grid.

--- submissions/Ban/vacuum2.py
+++ submissions/Ban/vacuum2.py
@@ -2,22 +2,48 @@ import agents as ag
 
 def HW2Agent() -> object:
     "An agent that keeps track of what locations are clean or dirty."
-    oldPercepts = [('None', 'Clean')]
+    oldPercepts = [('None', 'Clean', 'topNotFound', 'widthNotFound', 'moveLeft')]
     oldActions = ['NoOp']
 
     def program(percept):
         "Same as ReflexVacuumAgent, except if everything is clean, do NoOp."
         bump, status = percept
+        lastBump, lastStatus, topFound, widthFound, moveDirection = oldPercepts[-1]
+        lastAction = oldActions[-1]
         if status == 'Dirty':
             action = 'Suck'
         else:
-            lastBump, lastStatus = oldPercepts[-1]
-            if lastBump == 'None':
-                action = 'Right'
+            print (oldPercepts[-1])
+            if topFound == 'topNotFound' :
+                if lastBump == 'Bump' :
+                    action = 'Left'
+                    topFound = 'topFound'
+                else:
+                    action = 'Up'
+            elif moveDirection == 'moveRight':
+                print('inside the move right')
+                lastAction2 = oldActions[-2]
+                if lastAction2 == 'Left':
+                    action = 'Right'
+                elif lastBump == 'Bump':
+                    moveDirection = 'moveLeft'
+                    action = 'Down'
+                else:
+                    action = 'Right'
+            elif moveDirection == 'moveLeft' :
+                print('inside the move left')
+                lastAction2 = oldActions[-2]
+                if lastAction2 == 'Up' or lastAction == 'Down' :
+                    action = 'Left'
+                elif lastBump == 'Bump' :
+                    moveDirection = 'moveRight'
+                    action = 'Right'
+                else:
+                    action = 'Left'
             else:
-                action = 'Left'
-
-        oldPercepts.append(percept)
+                action = 'Right'
+        oldPercepts.append([bump, status, topFound, widthFound, moveDirection])
         oldActions.append(action)
+        # print(lastBump, lastStatus, topFound, widthFound, moveDirection)
         return action
     return ag.Agent(program)
