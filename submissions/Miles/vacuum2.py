c430a74c677ae163cc13e2d79e801cd7c1040ac5
abbeymiles <abbey.miles@pop.belmont.edu>
2016-09-06 21:46:34 -0500

    commit #3 for hw 2

--- submissions/Miles/vacuum2.py
+++ submissions/Miles/vacuum2.py
@@ -2,43 +2,45 @@ import agents as ag
 
 def HW2Agent() -> object:
     "An agent that keeps track of what locations are clean or dirty."
-    oldPercepts = [('None', 'Clean', 'Right', 'Left', 'Up', 'Down')]
+    oldPercepts = [('None', 'Clean', 'Right', 'Left', 'Up', 'Down', 'Top')]
     oldActions = ['NoOp']
 
     def program(percept):
         "Same as ReflexVacuumAgent, except if everything is clean, do NoOp."
         wall, status = percept
         lastLoc = oldPercepts[-1]
+
         lastAction = oldActions[-1]
         # checks to see if the starting point is dirty
         if status == 'Dirty':
             action = 'Suck'
         else:
 
-            if lastLoc == 'Up':
+            if atTop == 'Up':
                 if wall == 'Bump':  # checks to see if it hits the top
-                    action = 'Down'
                     action = 'Left'
+                    # topFound = 'topFound'
  # lastAction = 'Bump'
+
                 else:
                     action = 'Up'
                     # moves for the vacuum at the latest location of right
             elif lastLoc == 'Right':
 
 
-                lastAction = oldActions[-1]
+                lastAction = oldActions[-2]
                 if lastAction == 'Left':
                     action = 'Right'
                 elif wall == 'Bump':
-                    lastLoc = 'Left'
+                    action = 'Left'
                     action = 'Down'
                 else:
                     action = 'Right'
 
                     # moves for the vacuum at the latest location of left
-            elif lastLoc == 'Left' :
+            elif lastLoc == 'Left':
 
-                lastAction = oldActions[-1]
+                lastAction = oldActions[-2]
                 if lastAction == 'Up' or lastAction == 'Down' :
                     action = 'Left'
                 elif wall == 'Bump' :
