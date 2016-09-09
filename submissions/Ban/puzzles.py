b3bf0ed789e9dc6fdf043911338bdd3931437191
austinban <banaustinban@gmail.com>
2016-09-09 12:24:40 -0500

    Modified the puzzle

--- submissions/Ban/puzzles.py
+++ submissions/Ban/puzzles.py
@@ -2,16 +2,15 @@ import search
 from math import(cos, pi)
 
 sumner_map = search.UndirectedGraph(dict(
-    Portland=dict(Mitchellville=7, Fairfield=17, Cottontown=18),
-    Cottontown=dict(Portland=18),
-    Fairfield=dict(Mitchellville=21, Portland=17),
-    Mitchellville=dict(Portland=7, Fairfield=21),
+    Dublin=dict(Mullingar=67, Nass=41),
+    Mullingar=dict(Dublin=67, Naas=59),
+    Naas=dict(Dublin=41, Mullingar=59)
 ))
 
-sumner_puzzle = search.GraphProblem('Cottontown', 'Mitchellville', sumner_map)
+sumner_puzzle = search.GraphProblem('Dublin', 'Mullinger', sumner_map)
 
 sumner_puzzle.description = '''
-An abbreviated map of Sumner County, TN.
+An abbreviated map of Eastern Ireland.
 This map is unique, to the best of my knowledge.
 '''
 
