d378bb9f1e6d0e2e4864f7313bfeab26e2988624
hooperw <william.hooper@belmont.edu>
2016-09-13 18:12:46 -0500

    Added a LightSwitch problem to the list, to illustrate the syntax of search problems other than Graph searches.

--- grading/puzzle-submissions.py
+++ grading/puzzle-submissions.py
@@ -3,6 +3,7 @@ import agents as ag
 import importlib
 import traceback
 import search
+from utils import(isnumber)
 
 class MyException(Exception):
     pass
@@ -13,13 +14,40 @@ roster = ['Ban','Becker','Blue','Capps','Conklin','Dickenson','Fritz',
           'aardvark','zzzsolutions',
           ]
 
+
+def print_table(table, header=None, sep='   ', numfmt='%g'):
+    """Print a list of lists as a table, so that columns line up nicely.
+    header, if specified, will be printed as the first row.
+    numfmt is the format for all numbers; you might want e.g. '%6.2f'.
+    (If you want different formats in different columns,
+    don't use print_table.) sep is the separator between columns."""
+    justs = ['rjust' if isnumber(x) else 'ljust' for x in table[0]]
+
+    if header:
+        r = 0
+        for row in header:
+            table.insert(r, row)
+            r += 1
+
+    table = [[numfmt.format(x) if isnumber(x) else x for x in row]
+             for row in table]
+
+    sizes = list(
+            map(lambda seq: max(map(len, seq)),
+                list(zip(*[map(str, row) for row in table]))))
+
+    for row in table:
+        print(sep.join(getattr(
+            str(x), j)(size) for (j, size, x) in zip(justs, sizes, row)))
+
+
 def compare_searchers(problems, header, searchers=[]):
     def do(searcher, problem):
         p = search.InstrumentedProblem(problem)
         goalNode = searcher(p)
         return p, goalNode.path_cost
     table = [[search.name(s)] + [do(s, p) for p in problems] for s in searchers]
-    search.print_table(table, header)
+    print_table(table, header)
 
 submissions = {}
 scores = {}
@@ -38,17 +66,25 @@ print('----------------------------------------')
 for student in submissions:
     scores[student] = []
     try:
-        print(student)
+        plist = submissions[student]
+        hlist = [[student],['']]
+        i = 0
+        for problem in plist:
+            try:
+                hlist[0].append(problem.label)
+            except:
+                hlist[0].append('Problem ' + str(i))
+            i += 1
+            hlist[1].append('(<succ/goal/stat/fina>, cost)')
         compare_searchers(
-            problems=submissions[student],
-            header=['',
-                    '(<succ/goal/stat/fina>, cost)'
-                    ],
+            problems=plist,
+            header=hlist,
             searchers=[
                 search.depth_first_graph_search,
                 search.breadth_first_search,
                 search.iterative_deepening_search,
                 search.uniform_cost_search,
+                search.astar_search,
             ]
         )
     except:
