cce454448bc76cbdb2453019a4d1bc8e25f0aec0
Andy <andy.porter@pop.belmont.edu>
2016-09-12 19:14:25 -0500

    Vacuum 2 moved to submisisons folder

--- submissions/Porter/puzzles.py
+++ submissions/Porter/puzzles.py
@@ -2,13 +2,16 @@ import search
 from math import(cos, pi)
 
 sumner_map = search.UndirectedGraph(dict(
-    #Portland=dict(Mitchellville=7, Fairfield=17, Cottontown=18),
-    #Cottontown=dict(Portland=18),
-    #Fairfield=dict(Mitchellville=21, Portland=17),
-    #Mitchellville=dict(Portland=7, Fairfield=21),
+    # Portland=dict(Mitchellville=7, Fairfield=17, Cottontown=18),
+    # Cottontown=dict(Portland=18),
+    # Fairfield=dict(Mitchellville=21, Portland=17),
+    # Mitchellville=dict(Portland=7, Fairfield=21),
+    Dallas=dict(Austin=50, Houston=100),
+    Austin=dict(Dallas=50, Houston=20),
+    Houston=dict(Dallas=100, Austin=20),
 ))
 
-sumner_puzzle = search.GraphProblem('Cottontown', 'Mitchellville', sumner_map)
+sumner_puzzle = search.GraphProblem('Dallas', 'Houston', sumner_map)
 
 sumner_puzzle.description = '''
 An abbreviated map of Sumner County, TN.
@@ -19,9 +22,3 @@ myPuzzles = [
     sumner_puzzle,
 ]
 
-texas_map = search.UndirectedGraph(dict(
-    Dallas = dict(Austin=50, Houston=100),
-    Austin = dict(Dallas = 50, Houston = 20),
-    Houston = dict(Dallas=100, Austin=20),
-))
-texas_puzzle = search.GraphProblem('Dallas', 'Houston', texas_map)
