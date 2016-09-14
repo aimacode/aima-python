a5b4b55e374d1059f3ddcaed4d05ecbdc1a2e92f
John Ottenlips <john@jammber.com>
2016-09-14 13:42:25 -0500

    BFS better than DFS

--- submissions/Ottenlips/puzzles.py
+++ submissions/Ottenlips/puzzles.py
@@ -1,20 +1,22 @@
 import search
 from math import(cos, pi)
 
-sumner_map = search.UndirectedGraph(dict(
-    Kirkwood=dict(Webster=10, Clayton=17, St_Louis=21),
+stl_map = search.UndirectedGraph(dict(
+    Kirkwood=dict(Webster=10, Clayton=17, Glendale=7),
     St_Louis=dict(Clayton=12),
-    Clayton=dict(Webster=14, Kirkwood=17),
+    Glendale=dict(St_Louis=19),
+    Clayton=dict(Webster=14, St_Louis=12, Kirkwood=17),
     Webster=dict(Kirkwood=10, Clayton=14),
 ))
 
-sumner_puzzle = search.GraphProblem('Webster', 'St_Louis', sumner_map)
 
-sumner_puzzle.description = '''
+stl_puzzle = search.GraphProblem('Kirkwood', 'St_Louis', stl_map)
+
+stl_puzzle.description = '''
 An abbreviated map of Sumner County, TN.
 This map is unique, to the best of my knowledge.
 '''
 
 myPuzzles = [
-    sumner_puzzle,
+    stl_puzzle,
 ]
