58681e174fefad793c763f9ebf4b929b3b3bf0c5
John Ottenlips <john@jammber.com>
2016-09-21 16:07:29 -0500

    first a*

--- submissions/Ottenlips/puzzles.py
+++ submissions/Ottenlips/puzzles.py
@@ -9,7 +9,10 @@ stl_map = search.UndirectedGraph(dict(
     Clayton=dict(Webster=14, St_Louis=12, Kirkwood=17),
     Webster=dict(Kirkwood=10, Clayton=14),
 ))
-
+stl_map.locations = dict(
+    St_Louis=(38.6270, 90.1994),Webster=(38.5926, 90.3573),Kirkwood=(38.5834, 90.4068),
+    Glendale=(38.5959, 90.3771), MapleWood=(38.6104, 90.3228), Clayton=(38.6426, 90.3237),
+)
 
 stl_puzzle = search.GraphProblem('Kirkwood', 'St_Louis', stl_map)
 
@@ -18,6 +21,31 @@ An abbreviated map of Sumner County, TN.
 This map is unique, to the best of my knowledge.
 '''
 
+
+class LightSwitch(search.Problem):
+    def actions(self, state):
+        return ['up', 'down']
+
+    def result(self, state, action):
+        if action == 'up':
+            return 'on'
+        else:
+            return 'off'
+
+    def goal_test(self, state):
+        return state == 'on'
+
+    def h(self, node):
+        state = node.state
+        if self.goal_test(state):
+            return 0
+        else:
+            return 1
+
+switch_puzzle = LightSwitch('off')
+switch_puzzle.label = 'Light Switch'
+
 myPuzzles = [
     stl_puzzle,
+    switch_puzzle,
 ]
