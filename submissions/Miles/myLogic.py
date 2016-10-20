12bcda5dab1667988676ba0476f2e83678a41ec1
abbeymiles <abbey.miles@pop.belmont.edu>
2016-10-19 23:08:52 -0500

    commit for logic hw

---
+++ submissions/Miles/myLogic.py
@@ -0,0 +1,78 @@
+# farmer = {
+#     'kb': '''
+# Farmer(Mac)
+# Rabbit(Pete)
+# Mother(MrsMac, Mac)
+# Mother(MrsRabbit, Pete)
+# (Rabbit(r) & Farmer(f)) ==> Hates(f, r)
+# (Mother(m, c)) ==> Loves(m, c)
+# (Mother(m, r) & Rabbit(r)) ==> Rabbit(m)
+# (Farmer(f)) ==> Human(f)
+# (Mother(m, h) & Human(h)) ==> Human(m)
+# ''',
+pets = {
+    'kb': '''
+Boy(Dan)
+Girl(Sally)
+Dog(Spot)
+Cat(Fluffy)
+Mouse(Jerry)
+Pet(Spot, Fluffy)
+Owner(Dan, Spot)
+Owner(Sally, Fluffy)
+Treat(Cheese)
+(Cat(c) & Dog(d)) ==> Hates(c, d)
+(Cat(c) & Mouse(m)) ==> Loves(c, m)
+(Boy(b) & Girl(g)) ==> Loves(b, g)
+(Mouse(m) & Cat(c) & Dog(d)) ==> Wants(Cheese(c))
+Mouse(m) ==> Loves(Cheese(c))
+Boy(b) ==> Buys(Dog(d))
+Girl(g) ==> Buys(Cat(c))
+Boy(b) ==> Eats(Cheese(c))
+Boy(b) & Girl(g) ==> Loves(b, g)
+Girl(g) & Boy(b) ==> Hates(g, b)
+Girl(g) & Boy(b) ==> Human(h)
+
+
+
+
+''',
+
+
+
+
+# Note that this order of conjuncts
+# would result in infinite recursion:
+# '(Human(h) & Mother(m, h)) ==> Human(m)'
+    'queries':'''
+
+Hates(x, y)
+Loves(x, y)
+Wants(x)
+Eats(x)
+Buys(x)
+Human(h)
+''',
+#    'limit': 1,
+}
+
+# weapons = {
+#     'kb': '''
+# (American(x) & Weapon(y) & Sells(x, y, z) & Hostile(z)) ==> Criminal(x)
+# Owns(Nono, M1)
+# Missile(M1)
+# (Missile(x) & Owns(Nono, x)) ==> Sells(West, x, Nono)
+# Missile(x) ==> Weapon(x)
+# Enemy(x, America) ==> Hostile(x)
+# American(West)
+# Enemy(Nono, America)
+# ''',
+#     'queries':'''
+# Criminal(x)
+# ''',
+# }
+
+Examples = {
+     'pets': pets,
+#     'weapons': weapons,
+}
