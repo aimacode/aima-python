0a7f8ad247f96078f9ebc6b3715cb43f138e81d9
SeanCameronConklin <sean.conklin@pop.belmont.edu>
2016-10-27 02:50:29 -0500

    Finalizing changes. Got some weird errors to where I couldn't run the submissions because I couldn't install importlib even though it was working earlier for other trials?

---
+++ submissions/Conklin/myLogic.py
@@ -0,0 +1,41 @@
+heroesAndVillains = {
+     'kb': '''
+ Protagonist(Geralt)
+ Protagonist(Tidus)
+ Protagonist(Dragonborn)
+ Protagonist(Dante)
+ Antagonist(Eredin)
+ Antagonist(Jecht)
+ Antagonist(Alduin)
+ Antagonist(Vergil)
+ mortalEnemies(Geralt, Eredin)
+ mortalEnemies(Tidus, Jecht)
+ mortalEnemies(Dragonborn, Alduin)
+ mortalEnemies(Dante, Vergil)
+
+
+ (Protagonist(p) & Antagonist(a)) ==> Conflict(p, a)
+ mortalEnemies(p,a) & Antagonist(a) ==> Protagonist(p))
+ Protagonist(p) & Protagonist(q) ==> goodAlliance(p,q)
+ Antagonist(a) & Antagonist(z) ==> badAlliance(a,z)
+
+
+
+
+ ''',
+
+     'queries':'''
+ Protagonist(p)
+ Antagonist(a)
+ mortalEnemies(p,a)
+ goodAlliance(p,q)
+ badAlliance(a,z)
+ ''',
+
+ }
+
+
+
+ Examples = {
+     'heroesAndVillains': heroesAndVillains,
+ }
