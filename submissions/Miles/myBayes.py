6415865e7cb147b2c3f60fa08d24681de075557a
abbeymiles <abbey.miles@pop.belmont.edu>
2016-11-02 16:05:29 -0500

    commit #4 for hw - works

--- submissions/Miles/myBayes.py
+++ submissions/Miles/myBayes.py
@@ -10,68 +10,40 @@ class DataFrame:
     target = []  # list of the target value
     target_names = []  # target labels
 
+
+
 foodFF = DataFrame()  # what I pass on to examples
+foodFF.data = []
+targetData = []
 
 '''
 Extract data from the CORGIS food.
 '''
-joint = {}
+
 
 food = food.get_reports()
 
-for fiber in food:
+for info in food:
     try:
-        foodIT = fiber["Data"]["Fiber"]
-        joint[foodIT] = {}
-        # joint[it]['IT']= it
-        # joint[it]['Data'] = vit
+        item = float(info["Data"]["Fat"]["Saturated Fat"])
+        targetData.append(item)
+
+        fiber = float(info["Data"]["Fiber"])
+        carbohydrate = float(info["Data"]["Carboydrate"]) # they misspelled carbohydrates LOL
+        water = float(info["Data"]["Water"])
+
+        foodFF.data.append([fiber, carbohydrate, water])
+
     except:
         traceback.print_exc()
 
-# demographics = county_demographics.get_all_counties()
-for item in food:
-    try:
-        # countyNames = county['County'].split()
-        # cName = ' '.join(countyNames[:-1])
-        # st = county['State']
-        # countyST = cName + st
-        # foodIT = item["Data"]["Carbohydrate"]
-        category = item["Category"]
-        description = item["Description"]
-        NDBN = item["Nutrient Data Bank Number"]
-        # vitamins = county2['Data']["Vitamins"]["Vitamin A - IU"]
-        # if foodIT in joint:
-        #     joint[foodIT]["Category"] = category
-        #     joint[foodIT]["Description"] = description
-        #     joint[foodIT]["Nutrient Data Bank Number"] = NDBN
-        #    # joint[foodIT]['Vitamin A'] = vitamins
-    except:
-        traceback.print_exc()
-#
-#
-#
-foodFF.data = []
-#
-# '''
-# Build the input frame, row by row.
-# '''
-# for foodIT in joint:
-#     # choose the input values
-#     foodFF.data.append([
-#         foodIT,
-#         # joint[foodIT]['Fiber'],
-#         joint[foodIT]['Category'],
-#         joint[foodIT]['Description'],
-#         joint[foodIT]['Nutrient Data Bank Number'],
-#
-#     ])
 
 foodFF.feature_names = [
 
+
     'Fiber',
-    'Category',
-    'Description',
-    'Nutrient Data Bank Number',
+    'Carbohydrates',
+    'water',
 ]
 #
 # '''
@@ -88,21 +60,30 @@ foodFF.feature_names = [
 # '''
 foodFF.target = []
 #
-def foodTarget(grams):
-    if grams > 20:
+
+
+def foodTarget(percentage):
+
+
+    if percentage > 2:
         return 1
     return 0
 
+for pre in targetData:
+    # choose the target
+    tt = foodTarget(pre)
+    foodFF.target.append(tt)
 # for countyST in intersection:
 #     # choose the target
 #     tt = trumpTarget(intersection[countyST]['Trump'])
 #     trumpECHP.target.append(tt)
 #
 foodFF.target_names = [
-    'Fiber <= 20',
-    'Fiber >  20',
+    'Fat <= 2%',
+    'Fat >  2%',
 ]
 
 Examples = {
     'Food': foodFF,
 }
+
