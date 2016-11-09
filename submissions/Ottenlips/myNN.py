99ff8f421785bf2651facfc689d87fdd1aa6b583
John Ottenlips <john@jammber.com>
2016-11-09 15:16:20 -0600

    new target and dataframe

--- submissions/Ottenlips/myNN.py
+++ submissions/Ottenlips/myNN.py
@@ -14,78 +14,102 @@ class DataFrame:
 
 bill = DataFrame()
 
-
 list_of_billionaire = billionaires.get_billionaires()
 
-def billTarget(string):
-    if(billionaires['demographics']['age']<30 and billionaires['demographics']['age'] != -1):
+
+def billtarget(billions):
+    if billions<3.0:
         return 1
-    elif(billionaires['demographics']['age'] != -1):
-        return 0
     else:
-        return 2
+        return 0
+
 
 for billionaires in list_of_billionaire:
     # print(billionaires['wealth']['type'])
-    #print(billionaires)
-    bill.target.append(billTarget(billionaires['demographics']['age']))
+    # print(billionaires)
+    bill.target.append(billtarget(billionaires['wealth']['worth in billions']))
     # bill.target.append(billionaires['wealth']['how']['inherited'])
     bill.data.append([
-
+        float(billionaires['demographics']['age']),
         float(billionaires['location']['gdp']),
-        float(billionaires['wealth']['worth in billions']),
         float(billionaires['rank']),
-           # billionaires['demographics']['age'],
-
     ])
 
 
 bill.feature_names = [
+    'age',
     'gdp of origin country',
-    'worth',
     'rank',
-    # 'age',
 ]
 
 bill.target_names = [
-    'old',
-    'young',
-    'age not listed'
+    'very rich',
+    'less rich',
 ]
-
 
 
 '''
 Make a customn classifier,
 '''
 mlpc = MLPClassifier(
-    # hidden_layer_sizes = (100,),
-    # activation = 'relu',
-    solver='sgd', # 'adam',
-    # alpha = 0.0001,
-    # batch_size='auto',
-    learning_rate = 'adaptive', # 'constant',
-    # power_t = 0.5,
-     max_iter = 1000, # 200,
-    # shuffle = True,
-    # random_state = None,
-    # tol = 1e-4,
-    # verbose = False,
-    # warm_start = False,
-    # momentum = 0.9,
-    # nesterovs_momentum = True,
-    # early_stopping = False,
-    # validation_fraction = 0.1,
-    # beta_1 = 0.9,
-    # beta_2 = 0.999,
-    # epsilon = 1e-8,
+            hidden_layer_sizes = (1000,),
+            # activation = 'relu',
+            solver='sgd',
+            #alpha = 0.0001,
+            # batch_size='auto',
+            learning_rate = 'adaptive',
+            # power_t = 0.5,
+            max_iter = 100, # 200,
+            # shuffle = True,
+            # random_state = None,
+            # tol = 1e-4,
+            # verbose = True,
+            # warm_start = False,
+            # momentum = 0.9,
+            # nesterovs_momentum = True,
+            # early_stopping = False,
+            # validation_fraction = 0.1,
+            # beta_1 = 0.9,
+            # beta_2 = 0.999,
+            # epsilon = 1e-8,
 )
+
 
 Examples = {
    'Bill': {
 
         'frame': bill,
 
-        'mlpc':mlpc
+        'mlpc': mlpc,
             },
+
 }
+
+#
+# billTwo = DataFrame()
+#
+#
+#
+# for billionaires in list_of_billionaire:
+#     # print(billionaires['wealth']['type'])
+#     #print(billionaires)
+#     billTwo.target.append(float(billionaires['wealth']['worth in billions']))
+#     # bill.target.append(billionaires['wealth']['how']['inherited'])
+#     billTwo.data.append([
+#         float(billionaires['location']['gdp']),
+#         float(billionaires['rank']),
+#         float(billionaires['demographics']['age']),
+#     ])
+#
+#
+#
+#
+# billTwo.feature_names = [
+#         'gdp of origin country',
+#         'rank',
+#         'age',
+#     ]
+#
+# billTwo.target_names = [
+#         'worth',
+#     ]
