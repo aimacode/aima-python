73d7d2280ebfdc4f321898d5f19694593655bcb4
joelamartina <joseph.lamartina>
2016-12-02 12:42:02 -0600

    Created myKmeans in the LaMartina directory. Cannot get any of the submission classes to work for anyone except Aardvark. Not sure what is happening.

--- submissions/LaMartina/Alphametic.py
+++ submissions/LaMartina/Alphametic.py
@@ -99,12 +99,14 @@ def setInputString(top, bottom, sum):
 
 root = Tk()
 root.title("Alphametic Solver")
+#root.geometry('900x300')
 #root.title("Feet to Meters")
 
 mainframe = ttk.Frame(root, padding="3 3 12 12")
 mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
 mainframe.columnconfigure(0, weight=1)
 mainframe.rowconfigure(0, weight=1)
+
 
 # feet = StringVar()
 # meters = StringVar()
