2eca549b70e0c3f18951e905c3858e10c12d5f72
hooperw <william.hooper@belmont.edu>
2016-12-05 22:12:30 -0600

    lego hello world

--- submissions/aardvark/myLegos.py
+++ submissions/aardvark/myLegos.py
@@ -6,10 +6,10 @@
 #     method=nxt.locator.Method(
 #         bluetooth=False, fantomusb=True, fantombt=False, usb=False))
 
-from ev3dev.auto import OUTPUT_A, Motor
+from ev3dev.auto import OUTPUT_D, LargeMotor
 import time
 
-m = Motor(OUTPUT_A)
+m = LargeMotor(OUTPUT_D)
 m.run_forever(duty_cycle_sp = 100)
 time.sleep(1)
 m.stop()
