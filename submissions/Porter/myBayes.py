e9432af0b778a05748dd43638d293bc10f72e00a
Andy <andy.porter@pop.belmont.edu>
2016-12-05 18:27:21 -0600

    Submission for Bayesian Network

--- submissions/Porter/myBayes.py
+++ submissions/Porter/myBayes.py
@@ -10,37 +10,68 @@ class DataFrame:
     target = []
     target_names = []
 
-billionairesPIG = DataFrame()
+Frame = DataFrame()
+Frame.data = []
 
 
 
-billionaireInfo = billionaires.get_billionaires()
+
+billionaires = billionaires.get_billionaires()
 
 
 '''
 Build the input frame, row by row.
 '''
-for countyST in intersection:
-    # choose the input values
-    billionairesPIG.data.append([
-        # countyST,
-        # intersection[countyST]['ST'],
-        # intersection[countyST]['Trump'],
-        intersection[countyST]['Elderly'],
-        intersection[countyST]['College'],
-        intersection[countyST]['Home'],
-        intersection[countyST]['Poverty'],
-    ])
+# for countyST in intersection:
+#     # choose the input values
+#     billionairesPIG.data.append([
+#         # countyST,
+#         # intersection[countyST]['ST'],
+#         # intersection[countyST]['Trump'],
+#         intersection[countyST]['Elderly'],
+#         intersection[countyST]['College'],
+#         intersection[countyST]['Home'],
+#         intersection[countyST]['Poverty'],
+#     ])
 
-billionairesPIG.feature_names = [
-    # 'countyST',
-    # 'ST',
-    # 'Trump',
-    'Political',
-    'Inherited',
-    'Gender',
+def gender(string):
+    if string == "male":
+        return 1
+    if string == "female":
+        return 2
+    else:
+        return 0
+def political(value):
+    if value:
+        return 1
+    else:
+        return 0
+# def worth(number):
+#     if value
+#         return 1
+#     else:
+#         return 0
 
-]
+
+
+for x in billionaires:
+    Frame.target.append(gender(x['demographics']['gender']))
+    if x['wealth']['worth in billions'] > 2:
+        Frame.data.append([
+        political(x['wealth']['how']['was political']),
+        1,
+            0
+        ])
+    else:
+        Frame.data.append([
+            political(x['wealth']['how']['was political']),
+            0,
+            1,
+        ])
+
+
+
+Frame.feature_names = ['Political','Worth > 2 billion','Worth <= 2 billion']
 
 '''
 Build the target list,
@@ -54,23 +85,20 @@ the segment into which the variable's value will fall.
 In this example, I'm breaking Trump's % into two
 arbitrary segments.
 '''
-billionairesPIG.target = []
+# billionairesPIG.target = []
 
-def trumpTarget(percentage):
-    if percentage > 45:
-        return 1
-    return 0
+# def trumpTarget(percentage):
+#     if percentage > 45:
+#         return 1
+#     return 0
 
-for countyST in intersection:
-    # choose the target
-    tt = trumpTarget(intersection[countyST]['Trump'])
-    trumpECHP.target.append(tt)
+# for countyST in intersection:
+#     # choose the target
+#     tt = trumpTarget(intersection[countyST]['Trump'])
+#     trumpECHP.target.append(tt)
 
-billionairesPIG.target_names = [
-    'New',
-    'Old',
-]
+Frame.target_names = ['Male', 'Female', 'Not Stated']
 
 Examples = {
-    'Billionaires': billionairesECHP,
+    'Billionaires': Frame
 }
