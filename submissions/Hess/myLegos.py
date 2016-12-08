f13a4f73f4dabac92e7e25ca946587acc7cd311a
Will Hess <willh@aloompa.com>
2016-12-08 13:26:00 -0600

    Lego Move Test

--- submissions/Hess/myLegos.py
+++ submissions/Hess/myLegos.py
@@ -1,20 +1,24 @@
-# import nxt
-#
-# # brick = nxt.locator.find_one_brick(name = 'Hooper')
-# b = nxt.locator.find_one_brick(
-#     name="NXT", strict=True,
-#     method=nxt.locator.Method(
-#         bluetooth=False, fantomusb=True, fantombt=False, usb=False))
-
-from ev3dev.auto import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, LargeMotor
+from ev3dev.auto import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, LargeMotor, MediumMotor
 import time
 import ev3dev.ev3 as ev3
 
+clawMotor = MediumMotor(OUTPUT_A)
+armMotor = LargeMotor(OUTPUT_B)
+baseMotor = LargeMotor(OUTPUT_C)
+
 ev3.Sound.speak("Hello, how are you").wait()
-mA = LargeMotor(OUTPUT_A)
-mB = LargeMotor(OUTPUT_B)
-mC = LargeMotor(OUTPUT_C)
-mD = LargeMotor(OUTPUT_D)
-m.run_forever(speed_sp = 360)
-time.sleep(1)
-m.stop()
+
+ev3.Sound.speak("Now moving base.").wait()
+baseMotor.run_forever(speed_sp = 500)
+time.sleep(2)
+baseMotor.stop()
+
+ev3.Sound.speak("Now moving arm.").wait()
+armMotor.run_forever(speed_sp = 500)
+time.sleep(2)
+armMotor.stop()
+
+ev3.Sound.speak("Now moving claw.").wait()
+clawMotor.run_forever(speed_sp = 500)
+time.sleep(2)
+clawMotor.stop()
