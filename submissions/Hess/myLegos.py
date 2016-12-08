4fffa631a20bf58b7594f5f0d9775b9cf5bb8a47
Will Hess <willh@aloompa.com>
2016-12-08 13:47:13 -0600

    Lego Move Test

--- submissions/Hess/myLegos.py
+++ submissions/Hess/myLegos.py
@@ -1,24 +1,44 @@
-from ev3dev.auto import OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, LargeMotor, MediumMotor
+from ev3dev.auto import OUTPUT_A, OUTPUT_B, OUTPUT_C, LargeMotor, MediumMotor
+from ev3dev.auto import INPUT_1, INPUT_2, INPUT_3, TouchSensor, ColorSensor
 import time
 import ev3dev.ev3 as ev3
 
-clawMotor = MediumMotor(OUTPUT_A)
-armMotor = LargeMotor(OUTPUT_B)
-baseMotor = LargeMotor(OUTPUT_C)
+class ClawBot:
+    def __init__(self):
+    baseTouch = TouchSensor(INPUT_1)
+    armTouch = TouchSensor(INPUT_2)
+    colorSensor = ColorSensor(INPUT_3)
+    clawMotor = MediumMotor(OUTPUT_A)
+    armMotor = LargeMotor(OUTPUT_B)
+    baseMotor = LargeMotor(OUTPUT_C)
+    ClawBot.reset(self)
 
-ev3.Sound.speak("Hello, how are you").wait()
 
-ev3.Sound.speak("Now moving base.").wait()
-baseMotor.run_forever(speed_sp = 500)
-time.sleep(2)
-baseMotor.stop()
+    def test(self):
+        ev3.Sound.speak("Hello, how are you").wait()
+        ev3.Sound.speak("Now moving base.").wait()
+        ClawBot.baseMotor.run_forever(speed_sp = 500)
+        time.sleep(2)
+        ClawBot.baseMotor.stop()
+        ev3.Sound.speak("Now moving arm.").wait()
+        ClawBot.armMotor.run_forever(speed_sp = 500)
+        time.sleep(2)
+        ClawBot.armMotor.stop()
+        ev3.Sound.speak("Now moving claw.").wait()
+        ClawBot.clawMotor.run_forever(speed_sp = 500)
+        time.sleep(2)
+        ClawBot.clawMotor.stop()
+        return 0
 
-ev3.Sound.speak("Now moving arm.").wait()
-armMotor.run_forever(speed_sp = 500)
-time.sleep(2)
-armMotor.stop()
-
-ev3.Sound.speak("Now moving claw.").wait()
-clawMotor.run_forever(speed_sp = 500)
-time.sleep(2)
-clawMotor.stop()
+    def reset(self):
+        armtouch = False
+        basetouch = False
+        while(basetouch == False):
+            ClawBot.baseMotor.run_forever(speep_sp = 360)
+            time.sleep(1)
+            basetouch = ClawBot.baseTouch.is_pressed()
+        while(armtouch == False):
+            ClawBot.armMotor.run_forever(speed_sp = -360)
+            time.sleep(1)
+            armtouch = ClawBot.armTouch.is_pressed()
+        return 0
