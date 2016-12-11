6b332741f6d30e058e97b4c49a2aa0973e44128b
joelamartina <joseph.lamartina>
2016-12-11 15:19:53 -0600

    Got it to work. Now just making adjustments to make it move far enough

--- submissions/LaMartina/myLegos.py
+++ submissions/LaMartina/myLegos.py
@@ -25,16 +25,16 @@ mright = LargeMotor(OUTPUT_C)
 # mgrab.run_timed(speed_sp = -360, time_sp = 400)
 
 #move the arm down
-mup.run_timed(speed_sp = 360, time_sp = 500)
+mup.run_timed(speed_sp = 360, time_sp = 600)
 #grab the object
 time.sleep(1)
-mgrab.run_timed(speed_sp = 360, time_sp = 400)
+mgrab.run_timed(speed_sp = 360, time_sp = 500)
 # move arm back up
 time.sleep(1)
-mup.run_timed(speed_sp = -360, time_sp = 500)
+mup.run_timed(speed_sp = -360, time_sp = 600)
 #move arm right
 time.sleep(1)
-mright.run_timed(speed_sp = 360, time_sp = 300)
+mright.run_timed(speed_sp = 360, time_sp = 600)
 #move arm down at target location
 time.sleep(1)
 mup.run_timed(speed_sp = 360, time_sp = 705)
@@ -43,6 +43,6 @@ time.sleep(1)
 mgrab.run_timed(speed_sp = -360, time_sp = 400)
 #reset position
 time.sleep(1)
-mup.run_timed(speed_sp = -360, time_sp = 705)
+mup.run_timed(speed_sp = -360, time_sp = 600)
 time.sleep(1)
-mright.run_timed(speed_sp =-360, time_sp = 300)
+mright.run_timed(speed_sp =-360, time_sp = 400)
