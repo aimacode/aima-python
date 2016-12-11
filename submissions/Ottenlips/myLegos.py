7748081783c48cf9728842d331ccb85532b5a594
John Ottenlips <john@jammber.com>
2016-12-11 12:57:07 -0600

    linux box with python code for legos

--- submissions/Ottenlips/myLegos.py
+++ submissions/Ottenlips/myLegos.py
@@ -16,6 +16,7 @@ import time
 
 left = LargeMotor(OUTPUT_D)
 right = LargeMotor(OUTPUT_C)
+
 claw = MediumMotor(OUTPUT_A)
 # ir = InfraredSensor(address="in4")
 # ir = ev3.InfraredSensor()
@@ -24,8 +25,6 @@ claw = MediumMotor(OUTPUT_A)
 # assert ir.connected
 
 cs = ev3.ColorSensor()
-
-
 
 
 
@@ -39,60 +38,47 @@ def drive(dist, speed):
     right.run_to_rel_pos(position_sp=turns, speed_sp=speed)
 
 def close_claw():
-    claw.run_to_abs_pos(position_sp=0, speed_sp=300)
+    claw.run_to_abs_pos(position_sp=900, speed_sp=300)
 
 def open_claw():
-    claw.run_to_abs_pos(position_sp=90, speed_sp=300)
+    claw.run_to_abs_pos(position_sp=-900, speed_sp=300)
+
+# drive(10,200)
+
 
 
 item_found = False
 item_reached = False
 target_found = False
 target_reached = False
-
-
-#
-# while not item_found:
-#     # spin(10, 400)
-#     print(ir)
-#     # print(ir.address)
-#     # print(ir.commands)
-#     # print(ir.driver_name)
-#     # print(ir.connected())
-#     # print(ir.device_index)
-#     # print(ir.mode())
-#     # print(ir.bin_data())
-#     # print(ir.proximity())
-#     time.sleep(1)
-#     if ir.proximity() < 100:
-#         print("found")
-#         item_found = True
-
-# while not item_reached:
-#     drive(5, 400)
-
-    # if ir.proximity() < 10:
-    #     item_reached = True
-
-while not target_found:
-    # spin(10, 400)
-    time.sleep(1)
-    # print(ir.value())
+open_claw()
+c = cs.color()
+while not item_found:
+    spin(-5,10)
     c = cs.color()
     print(c)
-    print(cs.ambient_light_intensity())
-    print(cs.reflected_light_intensity())
-    # if c == 6: # white
-        # target_found = True
+    if(c==6):
+        time.sleep(1)
+        drive(10,200)
+        time.sleep(1)
+        close_claw()
+        item_found = True
 
-# while not target_reached:
-#     drive(5, 400)
-#     if cs.reflected_light_intensity() < 10:
-#         target_reached = True
+time.sleep(1)
+spin(-180, 900)
+time.sleep(1)
+drive(20,200)
+time.sleep(1)
 
-open_claw()
-spin(90, 400)
-drive(40, 800)
+# drive(20,200)
+while not target_found:
+    drive(5,200)
+    c = cs.color()
+    print(c)
+    if(c==6):
+        open_claw()
+        drive(-30,200)
 
-close_claw()
+        target_found = True
+
 
