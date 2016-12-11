a7b4aa0e8a7adb5d941e33825c18768fc35566ad
joelamartina <joseph.lamartina>
2016-12-11 14:32:49 -0600

    Completely ruined the arms location by starting going up instead of down. Now I am confirming that positive speed moves to the right before doing anything else.

--- submissions/LaMartina/myLegos.py
+++ submissions/LaMartina/myLegos.py
@@ -10,26 +10,32 @@ from ev3dev.auto import OUTPUT_D, LargeMotor, OUTPUT_A, MediumMotor, OUTPUT_C
 import time
 
 mup = LargeMotor(OUTPUT_D)
+mgrab = MediumMotor(OUTPUT_A)
+mright = LargeMotor(OUTPUT_C)
 #print(m)
 # m.run_forever(speed_sp = 360)
 # time.sleep(1)
 # m.stop()
 # print('Hooray')
 
-#move the arm down
-mup.run_timed(speed_sp = -360, time_sp = 705)
-mgrab = MediumMotor(OUTPUT_A)
-#grab the object
-mgrab.run_timed(speed_sp = 360, time_sp = 450)
-#move arm back up
-mup.run_timed(speed_sp = -360, time_sp = 705)
-mright = LargeMotor(OUTPUT_C)
-#move arm right
-mright.run_timed(speed_sp = 360, time_sp = 1000)
-#move arm down at target location
-mup.run_timed(speed_sp = 360, time_sp = 705)
-#release the object
-mgrab.run_timed(speed_sp = -360, time_sp = 450)
-#reset position
-mup.run_timed(speed_sp = -360, time_sp = 705)
-mright.run_timed(speed_sp =-360, time_sp = 1000)
+mright.run_timed(speed_sp = 360, time_sp = 100)
+
+# mup.run_timed(speed_sp = -360, time_sp = 500)
+# mright.run_timed(speed_sp = -360, time_sp = 100)
+# mgrab.run_timed(speed_sp = -360, time_sp = 400)
+
+# #move the arm down
+# mup.run_timed(speed_sp = 360, time_sp = 705)
+# #grab the object
+# mgrab.run_timed(speed_sp = 360, time_sp = 450)
+# #move arm back up
+# mup.run_timed(speed_sp = -360, time_sp = 705)
+# #move arm right
+# mright.run_timed(speed_sp = 360, time_sp = 1000)
+# #move arm down at target location
+# mup.run_timed(speed_sp = 360, time_sp = 705)
+# #release the object
+# mgrab.run_timed(speed_sp = -360, time_sp = 450)
+# #reset position
+# mup.run_timed(speed_sp = -360, time_sp = 705)
+# mright.run_timed(speed_sp =-360, time_sp = 1000)
