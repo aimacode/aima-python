76ebcd416ee45ece77cbf9b6d0974bdabec5e1cf
WhittKinley <whittkinley@gmail.com>
2016-12-11 21:37:03 -0600

    added project to kinley/

--- submissions/Kinley/LegoTest.py
+++ submissions/Kinley/LegoTest.py
@@ -1,29 +1,7 @@
 from ev3dev.auto import OUTPUT_D, LargeMotor, OUTPUT_A, MediumMotor, OUTPUT_C
 import time
 
-mup = LargeMotor(OUTPUT_D)
-mgrab = MediumMotor(OUTPUT_A)
-mright = LargeMotor(OUTPUT_C)
+right = LargeMotor(OUTPUT_D)
 
-#move the arm down
-mup.run_timed(speed_sp = 360, time_sp = 600)
-#grab the object
+right.run_timed(speed_sp = 360, time_sp = 600)
 time.sleep(1)
-mgrab.run_timed(speed_sp = 720, time_sp = 500)
-# move arm back up
-time.sleep(1)
-mup.run_timed(speed_sp = -360, time_sp = 600)
-#move arm right
-time.sleep(1)
-mright.run_timed(speed_sp = 360, time_sp = 600)
-#move arm down at target location
-time.sleep(1)
-mup.run_timed(speed_sp = 360, time_sp = 705)
-#release the object
-time.sleep(1)
-mgrab.run_timed(speed_sp = -360, time_sp = 400)
-#reset position
-time.sleep(1)
-mup.run_timed(speed_sp = -360, time_sp = 600)
-time.sleep(1)
-mright.run_timed(speed_sp =-360, time_sp = 400)
