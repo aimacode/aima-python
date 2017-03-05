c9eab0f6c55bf850b31ee9bed20da91a61dd7412
lucasmoura <lucas.moura128@gmail.com>
2017-03-05 06:23:29 -0300

    Add tests to CSP class (#299)
    * Add tests to CSP class
    
    Add test for the following methods on CSP class:
    
    * assign
    * unassigns
    * nconflits
    * actions
    
    * Refactor some asserts in test_csp.py
    
    Some asserts were being done in the following way:
    assert (X is not None) == True
    
    Now, they are handled in the following way:
    assert X

--- tests/test_csp.py
+++ tests/test_csp.py
@@ -2,16 +2,62 @@ import pytest
 from csp import *   #noqa
 
 
+def test_csp_assign():
+    var = 10
+    val = 5
+    assignment = {}
+    australia.assign(var, val, assignment)
+
+    assert australia.nassigns == 1
+    assert assignment[var] == val
+
+
+def test_csp_unassign():
+    var = 10
+    assignment = {var: 5}
+    australia.unassign(var, assignment)
+
+    assert var not in assignment
+
+
+def test_csp_nconflits():
+    map_coloring_test = MapColoringCSP(list('RGB'), 'A: B C; B: C; C: ')
+    assignment = {'A': 'R', 'B': 'G'}
+    var = 'C'
+    val = 'R'
+    assert map_coloring_test.nconflicts(var, val, assignment) == 1
+
+    val = 'B'
+    assert map_coloring_test.nconflicts(var, val, assignment) == 0
+
+
+def test_csp_actions():
+    map_coloring_test = MapColoringCSP(list('123'), 'A: B C; B: C; C: ')
+
+    state = {'A': '1', 'B': '2', 'C': '3'}
+    assert map_coloring_test.actions(state) == []
+
+    state = {'A': '1', 'B': '3'}
+    assert map_coloring_test.actions(state) == [('C', '2')]
+
+    state = {'A': '1', 'C': '2'}
+    assert map_coloring_test.actions(state) == [('B', '3')]
+
+    state = {'A': '1'}
+    assert (map_coloring_test.actions(state) == [('C', '2'), ('C', '3')] or
+            map_coloring_test.actions(state) == [('B', '2'), ('B', '3')])
+
+
 def test_backtracking_search():
-    assert (backtracking_search(australia) is not None) == True
-    assert (backtracking_search(australia, select_unassigned_variable=mrv) is not None) == True
-    assert (backtracking_search(australia, order_domain_values=lcv) is not None) == True
-    assert (backtracking_search(australia, select_unassigned_variable=mrv,
-                                order_domain_values=lcv) is not None) == True
-    assert (backtracking_search(australia, inference=forward_checking) is not None) == True
-    assert (backtracking_search(australia, inference=mac) is not None) == True
-    assert (backtracking_search(usa, select_unassigned_variable=mrv,
-                                order_domain_values=lcv, inference=mac) is not None) == True
+    assert backtracking_search(australia)
+    assert backtracking_search(australia, select_unassigned_variable=mrv)
+    assert backtracking_search(australia, order_domain_values=lcv)
+    assert backtracking_search(australia, select_unassigned_variable=mrv,
+                               order_domain_values=lcv)
+    assert backtracking_search(australia, inference=forward_checking)
+    assert backtracking_search(australia, inference=mac)
+    assert backtracking_search(usa, select_unassigned_variable=mrv,
+                               order_domain_values=lcv, inference=mac)
 
 
 def test_universal_dict():
