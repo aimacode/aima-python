685e2a57d07cf199a437ec320cfcf6cd9187af5b
Lucas Moura <lucas.moura128@gmail.com>
2017-03-08 17:09:58 -0300

    Update compare function in learning.py

--- learning.py
+++ learning.py
@@ -1105,14 +1105,14 @@ def ContinuousXor(n):
 # ______________________________________________________________________________
 
 
-def compare(algorithms=[PluralityLearner, NaiveBayesLearner,
-                        NearestNeighborLearner, DecisionTreeLearner],
-            datasets=[iris, orings, zoo, restaurant, SyntheticRestaurant(20),
-                      Majority(7, 100), Parity(7, 100), Xor(100)],
-            k=10, trials=1):
+def compare(algorithms=[PluralityLearner, NaiveBayesLearner, NearestNeighborLearner,
+                        DecisionTreeLearner],
+            datasets=[iris, orings, zoo, restaurant, SyntheticRestaurant(20), Majority(7, 100),
+                      Parity(7, 100), Xor(100)],
+            k=10, size=3, trials=1):
     """Compare various learners on various datasets using cross-validation.
     Print results as a table."""
     print_table([[a.__name__.replace('Learner', '')] +
-                 [cross_validation(a, d, k, trials) for d in datasets]
+                 [cross_validation(a, size, d, k, trials) for d in datasets]
                  for a in algorithms],
-                header=[''] + [d.name[0:7] for d in datasets], numfmt='%.2f')
+                 header=[''] + [d.name[0:7] for d in datasets], numfmt='{:.2f}')
