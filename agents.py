2802f695aab1b33c200f321bd311a9c3da638cea
Kaivalya Rawal <kaivalyaraw@gmail.com>
2017-03-11 16:53:49 +0530

    Added 2D GUI IPython capability

--- agents.py
+++ agents.py
@@ -516,6 +516,106 @@ class Obstacle(Thing):
 class Wall(Obstacle):
     pass
 
+# ______________________________________________________________________________
+
+from ipythonblocks import BlockGrid
+from IPython.display import HTML, display
+from time import sleep
+
+class GraphicEnvironment(XYEnvironment):
+    def __init__(self, width=10, height=10, boundary=True, color={}, display=False):
+        """define all the usual XYEnvironment characteristics, 
+        but initialise a BlockGrid for GUI too"""
+        super().__init__(width, height)
+        self.grid = BlockGrid(width, height, fill=(200,200,200))
+        if display:
+            self.grid.show()
+            self.visible = True
+        else:
+            self.visible = False
+        self.bounded = boundary
+        self.colors = color
+    
+    #def list_things_at(self, location, tclass=Thing): # need to override because locations
+    #    """Return all things exactly at a given location."""
+    #    return [thing for thing in self.things
+    #            if thing.location == location and isinstance(thing, tclass)]
+    
+    def get_world(self):
+        """Returns all the items in the world in a format 
+        understandable by the ipythonblocks BlockGrid"""
+        result = []
+        x_start, y_start = (0, 0)
+        x_end, y_end = self.width, self.height
+        for x in range(x_start, x_end):
+            row = []
+            for y in range(y_start, y_end):
+                row.append(self.list_things_at([x, y]))
+            result.append(row)
+        return result
+    
+    """def run(self, steps=1000, delay=1):
+        "" "Run the Environment for given number of time steps, 
+        but update the GUI too." ""
+        for step in range(steps):
+            sleep(delay)
+            if self.visible:
+                self.reveal()
+            if self.is_done():
+                if self.visible:
+                    self.reveal()
+                return
+            self.step()
+        if self.visible:
+            self.reveal()
+    """
+    def run(self, steps=1000, delay=1):
+        """Run the Environment for given number of time steps, 
+        but update the GUI too."""
+        for step in range(steps):
+            self.update(delay)
+            if self.is_done():
+                break
+            self.step()
+        self.update(delay)
+    
+    def update(self, delay=1):
+        sleep(delay)
+        if self.visible:
+            self.conceal()
+            self.reveal()
+        else:
+            self.reveal()
+    
+    def reveal(self):
+        """display the BlockGrid for this world - the last thing to be added 
+        at a location defines the location color"""
+        #print("Grid={}".format(self.grid))
+        self.draw_world()
+        #if not self.visible == True:
+        #    self.grid.show()
+        self.grid.show()
+        self.visible == True
+    
+    def draw_world(self):
+        self.grid[:] = (200, 200, 200)
+        world = self.get_world()
+        #print("world {}".format(world))
+        for x in range(0, len(world)):
+            for y in range(0, len(world[x])):
+                if len(world[x][y]):
+                    self.grid[y, x] = self.colors[world[x][y][-1].__class__.__name__]
+                    #print('location: ({}, {}) got color: {}'
+                    #.format(y, x, self.colors[world[x][y][-1].__class__.__name__]))
+    
+    def conceal(self):
+        """hide the BlockGrid for this world"""
+        self.visible = False
+        display(HTML(''))
+    
+    
+    
+
 
 
 # ______________________________________________________________________________
