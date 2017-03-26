c9a007b0de22c555982d0f50cee4a825dc3eec16
Angira Sharma <sofmonk@gmail.com>
2017-03-26 18:44:54 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -846,20 +846,20 @@ def subst(s, x):
 
 
 def fol_fc_ask(KB, alpha):
-     """A simple forward-chaining algorithm. [Figure 9.3]"""
+    """A simple forward-chaining algorithm. [Figure 9.3]"""
     while True:
         new = []
         for rule in KB.clauses:
             p, q = parse_definite_clause(standardize_variables(rule))
             for p_ in KB.clauses:
                 if p != p_:
-                     for theta in (subst(theta, p) == subst(theta, p_)):
-                            q_ = subst(theta, q)
-                            if not unify(q_,KB.sentence in KB) or not unify(q_, new):
-                                new.append(q_)
-                                phi = unify(q_,alpha)
-                                if phi is not None:
-                                    return phi
+                    for theta in (subst(theta, p) == subst(theta, p_)):
+                        q_ = subst(theta, q)
+                        if not unify(q_,KB.sentence in KB) or not unify(q_, new):
+                            new.append(q_)
+                            phi = unify(q_,alpha)
+                            if phi is not None:
+                                return phi
         KB.tell(new)
         if new is None:
             break
