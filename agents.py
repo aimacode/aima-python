4892aa8b86f89dec6e3798cb7cb2eafed0e5d1cf
Antonis Maronikolakis <antmarakis@programmers.gr>
2017-06-07 22:59:54 +0300

    Update agents.py

--- agents.py
+++ agents.py
@@ -35,7 +35,7 @@ EnvCanvas ## Canvas to display the environment of an EnvGUI
 #
 # Speed control in GUI does not have any effect -- fix it.
 
-from grid import distance_squared, turn_heading
+from utils import distance_squared, turn_heading
 from statistics import mean
 
 import random
