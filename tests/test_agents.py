c321ac5c7c015707b0a34b6a7ea8973161d43fa3
Anthony Marakis <antmarakis@programmers.gr>
2017-06-23 02:34:27 +0300

    Minor Cosmetic Changes to Agents (#556)
    * Update agents.py
    
    * Update test_agents.py

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -7,15 +7,19 @@ def test_move_forward():
     d = Direction("up")
     l1 = d.move_forward((0, 0))
     assert l1 == (0, -1)
+
     d = Direction(Direction.R)
     l1 = d.move_forward((0, 0))
     assert l1 == (1, 0)
+
     d = Direction(Direction.D)
     l1 = d.move_forward((0, 0))
     assert l1 == (0, 1)
+
     d = Direction("left")
     l1 = d.move_forward((0, 0))
     assert l1 == (-1, 0)
+
     l2 = d.move_forward((1, 0))
     assert l2 == (0, 0)
 
@@ -26,21 +30,25 @@ def test_add():
     l2 = d + "left"
     assert l1.direction == Direction.R
     assert l2.direction == Direction.L
+
     d = Direction("right")
     l1 = d.__add__(Direction.L)
     l2 = d.__add__(Direction.R)
     assert l1.direction == "up"
     assert l2.direction == "down"
+
     d = Direction("down")
     l1 = d.__add__("right")
     l2 = d.__add__("left")
     assert l1.direction == Direction.L
     assert l2.direction == Direction.R
+
     d = Direction(Direction.L)
     l1 = d + Direction.R
     l2 = d + Direction.L
     assert l1.direction == Direction.U
     assert l2.direction == Direction.D
+
 
 def test_ReflexVacuumAgent() :
     # create an object of the ReflexVacuumAgent
@@ -54,6 +62,7 @@ def test_ReflexVacuumAgent() :
     # check final status of the environment
     assert environment.status == {(1,0):'Clean' , (0,0) : 'Clean'}
 
+
 def test_ModelBasedVacuumAgent() :
     # create an object of the ModelBasedVacuumAgent
     agent = ModelBasedVacuumAgent()
@@ -65,6 +74,7 @@ def test_ModelBasedVacuumAgent() :
     environment.run()
     # check final status of the environment
     assert environment.status == {(1,0):'Clean' , (0,0) : 'Clean'}
+
 
 def test_Agent():
     def constant_prog(percept):
