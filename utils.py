8f06c0ef7c4049055545cde40ac95afd39f922ab
Anthony Marakis <antmarakis@programmers.gr>
2017-07-21 18:40:58 +0300

    utils docstring fix

--- utils.py
+++ utils.py
@@ -68,7 +68,7 @@ def mode(data):
 
 
 def powerset(iterable):
-    """powerset([1,2,3]) --> () (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"""
+    """powerset([1,2,3]) --> (1,) (2,) (3,) (1,2) (1,3) (2,3) (1,2,3)"""
     s = list(iterable)
     return list(chain.from_iterable(combinations(s, r) for r in range(len(s)+1)))[1:]
 
