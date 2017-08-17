f5e5f3d86c3929894168a1a4bbd993651059f3a3
Anthony Marakis <antmarakis@programmers.gr>
2017-08-17 20:56:46 +0300

    remove duplicate psource

--- notebook.py
+++ notebook.py
@@ -47,23 +47,6 @@ def psource(*functions):
 # ______________________________________________________________________________
 
 
-def psource(*functions):
-    "Print the source code for the given function(s)."
-    source_code = '\n\n'.join(getsource(fn) for fn in functions)
-    try:
-        from pygments.formatters import HtmlFormatter
-        from pygments.lexers import PythonLexer
-        from pygments import highlight
-                              
-        display(HTML(highlight(source_code, PythonLexer(), HtmlFormatter(full=True))))
-    
-    except ImportError:
-        print(source_code)
-
-
-# ______________________________________________________________________________
-
-
 def show_iris(i=0, j=1, k=2):
     """Plots the iris dataset in a 3D plot.
     The three axes are given by i, j and k,
