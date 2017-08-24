fc1b8652a5549d74ac74b745d347062594b556e3
Anthony Marakis <antmarakis@programmers.gr>
2017-08-24 11:14:36 +0300

    Update Notebooks (#629)
    * Update notebook.py
    
    - remove duplicate psource
    - add section headers
    - add mdp visualization code
    
    * Update nlp_apps.ipynb
    
    * Update csp.ipynb
    
    * Update mdp.ipynb
    
    * Update games.ipynb

--- notebook.py
+++ notebook.py
@@ -5,16 +5,18 @@ from games import TicTacToe, alphabeta_player, random_player, Fig52Extended, inf
 from logic import parse_definite_clause, standardize_variables, unify, subst
 from learning import DataSet
 from IPython.display import HTML, display
-from collections import Counter
+from collections import Counter, defaultdict
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 import os, struct
 import array
+import time
 
 
 #______________________________________________________________________________
+# Magic Words
 
 
 def pseudocode(algorithm):
@@ -22,6 +24,7 @@ def pseudocode(algorithm):
     from urllib.request import urlopen
     from IPython.display import Markdown
 
+    algorithm = algorithm.replace(' ', '-')
     url = "https://raw.githubusercontent.com/aimacode/aima-pseudocode/master/md/{}.md".format(algorithm)
     f = urlopen(url)
     md = f.read().decode('utf-8')
@@ -43,25 +46,8 @@ def psource(*functions):
     except ImportError:
         print(source_code)
 
-
 # ______________________________________________________________________________
-
-
-def psource(*functions):
-    "Print the source code for the given function(s)."
-    source_code = '\n\n'.join(getsource(fn) for fn in functions)
-    try:
-        from pygments.formatters import HtmlFormatter
-        from pygments.lexers import PythonLexer
-        from pygments import highlight
-                              
-        display(HTML(highlight(source_code, PythonLexer(), HtmlFormatter(full=True))))
-    
-    except ImportError:
-        print(source_code)
-
-
-# ______________________________________________________________________________
+# Iris Visualization
 
 
 def show_iris(i=0, j=1, k=2):
@@ -106,6 +92,7 @@ def show_iris(i=0, j=1, k=2):
     plt.show()
 
 # ______________________________________________________________________________
+# MNIST
 
 
 def load_MNIST(path="aima-data/MNIST"):
@@ -191,6 +178,52 @@ def show_ave_MNIST(labels, images):
         plt.title(cls)
 
     plt.show()
+
+# ______________________________________________________________________________
+# MDP
+
+
+def make_plot_grid_step_function(columns, rows, U_over_time):
+    """ipywidgets interactive function supports single parameter as input.
+    This function creates and return such a function by taking as input
+    other parameters."""
+
+    def plot_grid_step(iteration):
+        data = U_over_time[iteration]
+        data = defaultdict(lambda: 0, data)
+        grid = []
+        for row in range(rows):
+            current_row = []
+            for column in range(columns):
+                current_row.append(data[(column, row)])
+            grid.append(current_row)
+        grid.reverse() # output like book
+        fig = plt.imshow(grid, cmap=plt.cm.bwr, interpolation='nearest')
+
+        plt.axis('off')
+        fig.axes.get_xaxis().set_visible(False)
+        fig.axes.get_yaxis().set_visible(False)
+
+        for col in range(len(grid)):
+            for row in range(len(grid[0])):
+                magic = grid[col][row]
+                fig.axes.text(row, col, "{0:.2f}".format(magic), va='center', ha='center')
+
+        plt.show()
+
+    return plot_grid_step
+
+def make_visualize(slider):
+    """Takes an input a sliderand returns callback function
+    for timer and animation."""
+
+    def visualize_callback(Visualize, time_step):
+        if Visualize is True:
+            for i in range(slider.min, slider.max + 1):
+                slider.value = i
+                time.sleep(float(time_step))
+
+    return visualize_callback
 
 # ______________________________________________________________________________
 
