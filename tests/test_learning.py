e7fd79f1af4bf66a50fdb76d93b4c7b502139185
Ayush Jain <ayushjain081@gmail.com>
2018-02-18 15:05:02 +0530

    Added tests for information_content
    Added some tests for information_content function from learning.py

--- tests/test_learning.py
+++ tests/test_learning.py
@@ -165,6 +165,13 @@ def test_decision_tree_learner():
     assert dTL([7.5, 4, 6, 2]) == "virginica"
 
 
+def test_information_content():
+    assert information_content([]) == 0
+    assert information_content([4]) == 0
+    assert information_content([5, 4, 0, 2, 5, 0]) == 1.9237949406953985
+    assert information_content([1.5, 2.5]) == 0.9544340029249649
+
+
 def test_random_forest():
     iris = DataSet(name="iris")
     rF = RandomForest(iris)
