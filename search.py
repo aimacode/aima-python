3990ad5eaa2a9d2f10719a6aef422ab97ebaa370
AngryCracker <ads6may@gmail.com>
2018-03-06 14:38:32 +0530

    Refactor EightPuzzle class

--- search.py
+++ search.py
@@ -406,101 +406,92 @@ def astar_search(problem, h=None):
 
 class EightPuzzle(Problem):
 
-    """The problem of sliding tiles numbered from 1 to 8 on a 3x3 board,
+    ''' The problem of sliding tiles numbered from 1 to 8 on a 3x3 board,
     where one of the squares is a blank. A state is represented as a 3x3 list,
-    where element at index i,j represents the tile number (0 if it's an empty square)."""
+    where element at index i,j represents the tile number (0 if it's an empty square) '''
  
-    def __init__(self, initial, goal=None):
-        if goal:
-            self.goal = goal
-        else:
-            self.goal = [ [0,1,2], 
-                          [3,4,5], 
-                          [6,7,8] ]
-        Problem.__init__(self, initial, goal)
+    def __init__(self, initial, goal=[1, 2, 3, 4, 5, 6, 7, 8, 0]):
+        ''' Define goal state and initialize a problem '''
+
+        self.goal = tuple(goal)
+        Problem.__init__(self, tuple(initial), tuple(goal))
     
     def find_blank_square(self, state):
-        """Return the index of the blank square in a given state"""
-        for row in len(state):
-            for column in len(row):
-                if state[row][column] == 0:
-                    index_blank_square = (row, column)
-        return index_blank_square
+        '''Return the index of the blank square in a given state'''
+
+        return state.index(0)
     
     def actions(self, state):
-        """Return the actions that can be executed in the given state.
+        ''' Return the actions that can be executed in the given state.
         The result would be a list, since there are only four possible actions
-        in any given state of the environment."""
-       
-        possible_actions = list()
+        in any given state of the environment '''
+        
+        possible_actions = ['UP', 'DOWN', 'LEFT', 'RIGHT']       
         index_blank_square = self.find_blank_square(state)
 
-        if index_blank_square(0) == 0:
-            possible_actions += ['DOWN']
-        elif index_blank_square(0) == 1:
-            possible_actions += ['UP', 'DOWN']
-        elif index_blank_square(0) == 2:
-            possible_actions += ['UP']
-        
-        if index_blank_square(1) == 0:
-            possible_actions += ['RIGHT']
-        elif index_blank_square(1) == 1:
-            possible_actions += ['LEFT', 'RIGHT']
-        elif index_blank_square(1) == 2:
-            possible_actions += ['LEFT']
+        if index_blank_square % 3 == 0:
+            possible_actions.remove('LEFT')
+        if index_blank_square < 3:
+            possible_actions.remove('UP')
+        if index_blank_square % 3 == 2:
+            possible_actions.remove('RIGHT')
+        if index_blank_square > 5:
+            possible_actions.remove('DOWN')
 
         return possible_actions
 
     def result(self, state, action):
-        """Given state and action, return a new state that is the result of the action.
-        Action is assumed to be a valid action in the state."""
+        ''' Given state and action, return a new state that is the result of the action.
+        Action is assumed to be a valid action in the state '''
 
-        blank_square = self.find_blank_square(state)
-        new_state = [row[:] for row in state]
+        # ix is the index of the blank square
+        ix = self.find_blank_square(state)
+        new_state = list(state)
 
-        if action=='UP':
-            new_state[blank_square(0)][blank_square(1)] = new_state[blank_square(0)-1][blank_square(1)]
-            new_state[blank_square(0)-1][blank_square(1)] = 0
-        elif action=='LEFT':
-            new_state[blank_square(0)][blank_square(1)] = new_state[blank_square(0)][blank_square(1)-1]
-            new_state[blank_square(0)][blank_square(1)-1] = 0
-        elif action=='DOWN':
-            new_state[blank_square(0)][blank_square(1)] = new_state[blank_square(0)+1][blank_square(1)]
-            new_state[blank_square(0)+1][blank_square(1)] = 0
-        elif action=='RIGHT':
-            new_state[blank_square(0)][blank_square(1)] = new_state[blank_square(0)][blank_square(1)+1]
-            new_state[blank_square(0)][blank_square(1)+1] = 0
+        if action == 'UP':
+            new_state[ix], new_state[ix - 3] = new_state[ix - 3], 0
+        elif action == 'DOWN':
+            new_state[ix], new_state[ix + 3] = new_state[ix + 3], 0
+        elif action == 'LEFT':
+            new_state[ix], new_state[ix - 1] = new_state[ix - 1], 0
+        elif action == 'RIGHT':
+            new_state[ix], new_state[ix + 1] = new_state[ix + 1], 0
         else:
-            print("Invalid Action!")
-        return new_state
+            print('Invalid Action')
+
+        return tuple(new_state)
 
     def goal_test(self, state):
-        """Given a state, return True if state is a goal state or False, otherwise"""
-        for row in len(state):
-            for column in len(row):
-                if state[row][col] != self.goal[row][column]:
-                    return False
-        return True
+        ''' Given a state, return True if state is a goal state or False, otherwise '''
 
-    def checkSolvability(self, state):
+        if tuple(state) == self.goal:
+            return True
+        return False
+
+    def check_solvability(self, state):
+        ''' Checks if the given state is solvable '''
+
         inversion = 0
         for i in range(len(state)):
-               for j in range(i, len(state)):
-                    if (state[i] > state[j] and state[j] != 0):
-                                    inversion += 1
+            for j in range(i, len(state)):
+                if (state[i] > state[j] and state[j] != 0):
+                    inversion += 1
         check = True
-        if inversion%2 != 0:
-                check = False
+
+        if inversion % 2 != 0:
+            check = False
         print(check)
     
-    def h(self, state):
-        """Return the heuristic value for a given state. Heuristic function used is 
-        h(n) = number of misplaced tiles."""
+    def h(self, node):
+        ''' Return the heuristic value for a given state. Default heuristic function used is 
+        h(n) = number of misplaced tiles '''
+
         num_misplaced_tiles = 0
-        for row in len(state):
-            for column in len(row):
-                if state[row][col] != self.goal[row][column]:
-                    num_misplaced_tiles += 1
+
+        for i in range(len(node.state)):
+            if node.state[i] != self.goal[i]:
+                num_misplaced_tiles += 1
+
         return num_misplaced_tiles
 
 # ______________________________________________________________________________
