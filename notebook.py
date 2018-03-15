a352afa750a06c0c38eba9b211830e2f9ba92679
Krishna Wadhwani <krishnaw14@gmail.com>
2018-03-15 18:12:12 +0530

    Update notebook.py
    Bringing up to date with original repo

--- notebook.py
+++ notebook.py
@@ -9,6 +9,7 @@ from collections import Counter, defaultdict
 
 import matplotlib.pyplot as plt
 import numpy as np
+from PIL import Image
 
 import os, struct
 import array
@@ -1042,3 +1043,30 @@ def display_visual(graph_data, user_input, algorithm=None, problem=None):
         slider = widgets.IntSlider(min=0, max=1, step=1, value=0)
         slider_visual = widgets.interactive(slider_callback, iteration=slider)
         display(slider_visual)
+
+
+# Function to plot NQueensCSP in csp.py and NQueensProblem in search.py
+def plot_NQueens(solution):
+    n = len(solution)
+    board = np.array([2 * int((i + j) % 2) for j in range(n) for i in range(n)]).reshape((n, n))        
+    im = Image.open('images/queen_s.png')
+    height = im.size[1]
+    im = np.array(im).astype(np.float) / 255
+    fig = plt.figure(figsize=(7, 7))
+    ax = fig.add_subplot(111)
+    ax.set_title('{} Queens'.format(n))
+    plt.imshow(board, cmap='binary', interpolation='nearest')
+    # NQueensCSP gives a solution as a dictionary
+    if isinstance(solution, dict):
+        for (k, v) in solution.items():
+            newax = fig.add_axes([0.064 + (k * 0.112), 0.062 + ((7 - v) * 0.112), 0.1, 0.1], zorder=1)
+            newax.imshow(im)
+            newax.axis('off')
+    # NQueensProblem gives a solution as a list
+    elif isinstance(solution, list):
+        for (k, v) in enumerate(solution):
+            newax = fig.add_axes([0.064 + (k * 0.112), 0.062 + ((7 - v) * 0.112), 0.1, 0.1], zorder=1)
+            newax.imshow(im)
+            newax.axis('off')
+    fig.tight_layout()
+    plt.show()
