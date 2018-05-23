227848a04e2663af49845b280bbde4698c49af08
AngryCracker <ads6may@gmail.com>
2018-05-23 21:48:00 +0530

    Added PartialOrderPlanner

--- planning.py
+++ planning.py
@@ -5,8 +5,9 @@ import copy
 import itertools
 from search import Node
 from utils import Expr, expr, first
-from logic import FolKB, conjuncts
+from logic import FolKB, conjuncts, unify
 from collections import deque
+from functools import reduce as _reduce
 
 
 class PDDL:
@@ -84,6 +85,9 @@ class Action:
     def __call__(self, kb, args):
         return self.act(kb, args)
 
+    def __repr__(self):
+        return '{}({})'.format(self.__class__.__name__, Expr(self.name, *self.args))
+
     def convert(self, clauses):
         """Converts strings into Exprs"""
         if isinstance(clauses, Expr):
@@ -152,7 +156,7 @@ def air_cargo():
     """Air cargo problem"""
 
     return PDDL(init='At(C1, SFO) & At(C2, JFK) & At(P1, SFO) & At(P2, JFK) & Cargo(C1) & Cargo(C2) & Plane(P1) & Plane(P2) & Airport(SFO) & Airport(JFK)', 
-                goals='At(C1, JFK) & At(C2, SFO)', 
+                goals='At(C1, JFK) & At(C2, SFO)',
                 actions=[Action('Load(c, p, a)', 
                                 precond='At(c, a) & At(p, a) & Cargo(c) & Plane(p) & Airport(a)',
                                 effect='In(c, p) & ~At(c, a)'),
@@ -192,6 +196,19 @@ def three_block_tower():
                          Action('MoveToTable(b, x)',
                                 precond='On(b, x) & Clear(b) & Block(b)',
                                 effect='On(b, Table) & Clear(x) & ~On(b, x)')])
+
+
+def simple_blocks_world():
+    """Simplified blocks world"""
+
+    return PDDL(init='On(A, B) & Clear(A) & OnTable(B) & OnTable(C) & Clear(C)',
+                goals='On(B, A) & On(C, B)',
+                actions=[Action('ToTable(x, y)',
+                                precond='On(x, y) & Clear(x)',
+                                effect='~On(x, y) & Clear(y) & OnTable(x)'),
+                         Action('FromTable(y, x)',
+                                precond='OnTable(y) & Clear(y) & Clear(x)',
+                                effect='~OnTable(y) & ~Clear(x) & On(y, x)')])
 
 
 def have_cake_and_eat_cake_too():
@@ -239,8 +256,9 @@ def socks_and_shoes():
                                 effect='LeftSockOn')])
 
 
-# Doubles tennis problem
 def double_tennis_problem():
+    """Doubles tennis problem"""
+
     return PDDL(init='At(A, LeftBaseLine) & At(B, RightNet) & Approaching(Ball, RightBaseLine) & Partner(A, B) & Partner(B, A)',
                              goals='Returned(Ball) & At(a, LeftNet) & At(a, RightNet)',
                              actions=[Action('Hit(actor, Ball, loc)',
@@ -573,6 +591,351 @@ def linearize(solution):
     return linear_solution
 
 
+'''
+[10.13 PartialOrderPlanner: Partially ordered plans are created by a search through the space of plans
+rather than a search through the state space. It views planning as a refinement of partially ordered plans.
+A partially ordered plan is defined by a set of actions and a set of constraints of the form A < B,
+which denotes that action A has to be performed before action B.]
+'''
+
+class PartialOrderPlanner:
+
+    def __init__(self, pddl):
+        self.pddl = pddl
+        self.initialize()
+
+    def initialize(self):
+        """Initialize all variables"""
+        self.causal_links = []
+        self.start = Action('Start', [], self.pddl.init)
+        self.finish = Action('Finish', self.pddl.goals, [])
+        self.actions = set()
+        self.actions.add(self.start)
+        self.actions.add(self.finish)
+        self.constraints = set()
+        self.constraints.add((self.start, self.finish))
+        self.agenda = set()
+        for precond in self.finish.precond:
+            self.agenda.add((precond, self.finish))
+        self.expanded_actions = self.expand_actions()
+
+    def expand_actions(self, name=None):
+        """Generate all possible actions with variable bindings for precondition selection heuristic"""
+
+        objects = set(arg for clause in self.pddl.init for arg in clause.args)
+        expansions = []
+        action_list = []
+        if name is not None:
+            for action in self.pddl.actions:
+                if str(action.name) == name:
+                    action_list.append(action)
+        else:
+            action_list = self.pddl.actions
+
+        for action in action_list:
+            for permutation in itertools.permutations(objects, len(action.args)):
+                bindings = unify(Expr(action.name, *action.args), Expr(action.name, *permutation))
+                if bindings is not None:
+                    new_args = []
+                    for arg in action.args:
+                        if arg in bindings:
+                            new_args.append(bindings[arg])
+                        else:
+                            new_args.append(arg)
+                    new_expr = Expr(str(action.name), *new_args)
+                    new_preconds = []
+                    for precond in action.precond:
+                        new_precond_args = []
+                        for arg in precond.args:
+                            if arg in bindings:
+                                new_precond_args.append(bindings[arg])
+                            else:
+                                new_precond_args.append(arg)
+                        new_precond = Expr(str(precond.op), *new_precond_args)
+                        new_preconds.append(new_precond)
+                    new_effects = []
+                    for effect in action.effect:
+                        new_effect_args = []
+                        for arg in effect.args:
+                            if arg in bindings:
+                                new_effect_args.append(bindings[arg])
+                            else:
+                                new_effect_args.append(arg)
+                        new_effect = Expr(str(effect.op), *new_effect_args)
+                        new_effects.append(new_effect)
+                    expansions.append(Action(new_expr, new_preconds, new_effects))
+
+        return expansions
+
+    def find_open_precondition(self):
+        """Find open precondition with the least number of possible actions"""
+
+        number_of_ways = dict()
+        actions_for_precondition = dict()
+        for element in self.agenda:
+            open_precondition = element[0]
+            possible_actions = list(self.actions) + self.expanded_actions
+            for action in possible_actions:
+                for effect in action.effect:
+                    if effect == open_precondition:
+                        if open_precondition in number_of_ways:
+                            number_of_ways[open_precondition] += 1
+                            actions_for_precondition[open_precondition].append(action)
+                        else:
+                            number_of_ways[open_precondition] = 1
+                            actions_for_precondition[open_precondition] = [action]
+
+        number = sorted(number_of_ways, key=number_of_ways.__getitem__)
+        
+        for k, v in number_of_ways.items():
+            if v == 0:
+                return None, None, None
+
+        act1 = None
+        for element in self.agenda:
+            if element[0] == number[0]:
+                act1 = element[1]
+                break
+
+        if number[0] in self.expanded_actions:
+            self.expanded_actions.remove(number[0])
+
+        return number[0], act1, actions_for_precondition[number[0]]
+
+    def find_action_for_precondition(self, oprec):
+        """Find action for a given precondition"""
+
+        # either
+        #   choose act0 E Actions such that act0 achieves G
+        for action in self.actions:
+            for effect in action.effect:
+                if effect == oprec:
+                    return action, 0
+
+        # or
+        #   choose act0 E Actions such that act0 achieves G
+        for action in self.pddl.actions:
+            for effect in action.effect:
+                if effect.op == oprec.op:
+                    bindings = unify(effect, oprec)
+                    if bindings is None:
+                        break
+                    return action, bindings
+
+    def generate_expr(self, clause, bindings):
+        """Generate atomic expression from generic expression given variable bindings"""
+
+        new_args = []
+        for arg in clause.args:
+            if arg in bindings:
+                new_args.append(bindings[arg])
+            else:
+                new_args.append(arg)
+
+        try:
+            return Expr(str(clause.name), *new_args)
+        except:
+            return Expr(str(clause.op), *new_args)
+        
+    def generate_action_object(self, action, bindings):
+        """Generate action object given a generic action andvariable bindings"""
+
+        # if bindings is 0, it means the action already exists in self.actions
+        if bindings == 0:
+            return action
+
+        # bindings cannot be None
+        else:
+            new_expr = self.generate_expr(action, bindings)
+            new_preconds = []
+            for precond in action.precond:
+                new_precond = self.generate_expr(precond, bindings)
+                new_preconds.append(new_precond)
+            new_effects = []
+            for effect in action.effect:
+                new_effect = self.generate_expr(effect, bindings)
+                new_effects.append(new_effect)
+            return Action(new_expr, new_preconds, new_effects)
+
+    def cyclic(self, graph):
+        """Check cyclicity of a directed graph"""
+
+        new_graph = dict()
+        for element in graph:
+            if element[0] in new_graph:
+                new_graph[element[0]].append(element[1])
+            else:
+                new_graph[element[0]] = [element[1]]
+
+        path = set()
+
+        def visit(vertex):
+            path.add(vertex)
+            for neighbor in new_graph.get(vertex, ()):
+                if neighbor in path or visit(neighbor):
+                    return True
+            path.remove(vertex)
+            return False
+
+        value = any(visit(v) for v in new_graph)
+        return value
+
+    def add_const(self, constraint, constraints):
+        """Add the constraint to constraints if the resulting graph is acyclic"""
+
+        if constraint[0] == self.finish or constraint[1] == self.start:
+            return constraints
+
+        new_constraints = set(constraints)
+        new_constraints.add(constraint)
+
+        if self.cyclic(new_constraints):
+            return constraints
+        return new_constraints
+
+    def is_a_threat(self, precondition, effect):
+        """Check if effect is a threat to precondition"""
+
+        if (str(effect.op) == 'Not' + str(precondition.op)) or ('Not' + str(effect.op) == str(precondition.op)):
+            if effect.args == precondition.args:
+                return True
+        return False
+
+    def protect(self, causal_link, action, constraints):
+        """Check and resolve threats by promotion or demotion"""
+
+        threat = False
+        for effect in action.effect:
+            if self.is_a_threat(causal_link[1], effect):
+                threat = True
+                break
+
+        if action != causal_link[0] and action != causal_link[2] and threat:
+            # try promotion
+            new_constraints = set(constraints)
+            new_constraints.add((action, causal_link[0]))
+            if not self.cyclic(new_constraints):
+                constraints = self.add_const((action, causal_link[0]), constraints)
+            else:
+                # try demotion
+                new_constraints = set(constraints)
+                new_constraints.add((causal_link[2], action))
+                if not self.cyclic(new_constraints):
+                    constraints = self.add_const((causal_link[2], action), constraints)
+                else:
+                    # both promotion and demotion fail
+                    print('Unable to resolve a threat caused by', action, 'onto', causal_link)
+                    return
+        return constraints
+
+    def convert(self, constraints):
+        """Convert constraints into a dict of Action: set() orderings"""
+
+        graph = dict()
+        for constraint in constraints:
+            if constraint[0] in graph:
+                graph[constraint[0]].add(constraint[1])
+            else:
+                graph[constraint[0]] = set()
+                graph[constraint[0]].add(constraint[1])
+        return graph
+
+    def toposort(self, graph):
+        """Generate topological ordering of constraints"""
+
+        if len(graph) == 0:
+            return
+
+        graph = graph.copy()
+
+        for k, v in graph.items():
+            v.discard(k)
+
+        extra_elements_in_dependencies = _reduce(set.union, graph.values()) - set(graph.keys())
+
+        graph.update({element:set() for element in extra_elements_in_dependencies})
+        while True:
+            ordered = set(element for element, dependency in graph.items() if len(dependency) == 0)
+            if not ordered:
+                break
+            yield ordered
+            graph = {element: (dependency - ordered) for element, dependency in graph.items() if element not in ordered}
+        if len(graph) != 0:
+            raise ValueError('The graph is not acyclic and cannot be linearly ordered')
+
+    def display_plan(self):
+        """Display causal links, constraints and the plan"""
+
+        print('Causal Links')
+        for causal_link in self.causal_links:
+            print(causal_link)
+
+        print('\nConstraints')
+        for constraint in self.constraints:
+            print(constraint[0], '<', constraint[1])
+
+        print('\nPartial Order Plan')
+        print(list(reversed(list(self.toposort(self.convert(self.constraints))))))
+
+    def execute(self, display=True):
+        """Execute the algorithm"""
+
+        step = 1
+        self.tries = 1
+        while len(self.agenda) > 0:
+            step += 1
+            # select <G, act1> from Agenda
+            try:
+                G, act1, possible_actions = self.find_open_precondition()
+            except IndexError:
+                print('Probably Wrong')
+                break
+
+            act0 = possible_actions[0]
+            # remove <G, act1> from Agenda
+            self.agenda.remove((G, act1))
+
+            # For actions with variable number of arguments, use least commitment principle
+            # act0_temp, bindings = self.find_action_for_precondition(G)
+            # act0 = self.generate_action_object(act0_temp, bindings)
+
+            # Actions = Actions U {act0}
+            self.actions.add(act0)
+
+            # Constraints = add_const(start < act0, Constraints)
+            self.constraints = self.add_const((self.start, act0), self.constraints)
+
+            # for each CL E CausalLinks do
+            #   Constraints = protect(CL, act0, Constraints)
+            for causal_link in self.causal_links:
+                self.constraints = self.protect(causal_link, act0, self.constraints)
+
+            # Agenda = Agenda U {<P, act0>: P is a precondition of act0}
+            for precondition in act0.precond:
+                self.agenda.add((precondition, act0))
+
+            # Constraints = add_const(act0 < act1, Constraints)
+            self.constraints = self.add_const((act0, act1), self.constraints)
+
+            # CausalLinks U {<act0, G, act1>}
+            if (act0, G, act1) not in self.causal_links:
+                self.causal_links.append((act0, G, act1))
+
+            # for each A E Actions do
+            #   Constraints = protect(<act0, G, act1>, A, Constraints)
+            for action in self.actions:
+                self.constraints = self.protect((act0, G, act1), action, self.constraints)
+
+            if step > 200:
+                print('Couldn\'t find a solution')
+                return None, None
+
+        if display:
+            self.display_plan()
+        else:
+            return self.constraints, self.causal_links                
+
+
 def spare_tire_graphplan():
     """Solves the spare tire problem using GraphPlan"""
     return GraphPlan(spare_tire()).execute()
@@ -596,6 +959,10 @@ def shopping_graphplan():
 def socks_and_shoes_graphplan():
     """Solves the socks and shoes problem using GraphpPlan"""
     return GraphPlan(socks_and_shoes()).execute()
+
+def simple_blocks_world_graphplan():
+    """Solves the simple blocks world problem"""
+    return GraphPlan(simple_blocks_world()).execute()
 
 
 class HLA(Action):
