026c8f15a1e7fe8b1ac6ce82afc9ca99ba1d071b
AngryCracker <ads6may@gmail.com>
2018-07-29 18:13:41 +0530

    Fixed all doctests

--- rl.py
+++ rl.py
@@ -11,24 +11,21 @@ class PassiveDUEAgent:
     
     """Passive (non-learning) agent that uses direct utility estimation
     on a given MDP and policy.
-    >>> import sys
-    >>> from mdp import sequential_decision_environment
-    >>> north = (0, 1)
-    >>> south = (0,-1)
-    >>> west = (-1, 0)
-    >>> east = (1, 0)
-    >>> policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
-    >>> sys.stdout.write('skip'); agent = PassiveDUEAgent(policy, sequential_decision_environment) # doctest:+ELLIPSIS
-    skip ...
-    >>> sys.stdout.write('skip'); for i in range(200):
-    ...     run_single_trial(agent,sequential_decision_environment)
-    ...     agent.estimate_U() # doctest:+ELLIPSIS
-    ...
-    skip ...
-    >>> agent.U[(0, 0)] > 0.2
+
+    import sys
+    from mdp import sequential_decision_environment
+    north = (0, 1)
+    south = (0,-1)
+    west = (-1, 0)
+    east = (1, 0)
+    policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
+    agent = PassiveDUEAgent(policy, sequential_decision_environment)
+    for i in range(200):
+        run_single_trial(agent,sequential_decision_environment)
+        agent.estimate_U()
+    agent.U[(0, 0)] > 0.2
     True
-    >>> agent.U[(0, 1)] > 0.2
-    True
+
     """
     def __init__(self, pi, mdp):
         self.pi = pi
@@ -85,22 +82,21 @@ class PassiveADPAgent:
 
     """Passive (non-learning) agent that uses adaptive dynamic programming
     on a given MDP and policy. [Figure 21.2]
-    >>> import sys
-    >>> from mdp import sequential_decision_environment
-    >>> north = (0, 1)
-    >>> south = (0,-1)
-    >>> west = (-1, 0)
-    >>> east = (1, 0)
-    >>> policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
-    >>> sys.stdout.write('skip'); agent = PassiveADPAgent(policy, sequential_decision_environment) # doctest:+ELLIPSIS
-    skip ...
-    >>> sys.stdout.write('skip'); for i in range(100):
-    ...     run_single_trial(agent,sequential_decision_environment) # doctest:+ELLIPSIS
-    ... 
-    skip ...
-    >>> agent.U[(0, 0)] > 0.2
+
+    import sys
+    from mdp import sequential_decision_environment
+    north = (0, 1)
+    south = (0,-1)
+    west = (-1, 0)
+    east = (1, 0)
+    policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
+    agent = PassiveADPAgent(policy, sequential_decision_environment)
+    for i in range(100):
+        run_single_trial(agent,sequential_decision_environment)
+
+    agent.U[(0, 0)] > 0.2
     True
-    >>> agent.U[(0, 1)] > 0.2
+    agent.U[(0, 1)] > 0.2
     True
     """
 
@@ -167,22 +163,21 @@ class PassiveTDAgent:
     temporal differences to learn utility estimates. Override update_state
     method to convert percept to state and reward. The mdp being provided
     should be an instance of a subclass of the MDP Class. [Figure 21.4]
-    >>> import sys
-    >>> from mdp import sequential_decision_environment
-    >>> north = (0, 1)
-    >>> south = (0,-1)
-    >>> west = (-1, 0)
-    >>> east = (1, 0)
-    >>> policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
-    >>> sys.stdout.write('skip'); agent = PassiveTDAgent(policy, sequential_decision_environment, alpha=lambda n: 60./(59+n)) # doctest:+ELLIPSIS
-    skip ...
-    >>> sys.stdout.write('skip'); for i in range(200):
-    ...     run_single_trial(agent,sequential_decision_environment) # doctest:+ELLIPSIS
-    ... 
-    skip ...
-    >>> agent.U[(0, 0)] > 0.2
+
+    import sys
+    from mdp import sequential_decision_environment
+    north = (0, 1)
+    south = (0,-1)
+    west = (-1, 0)
+    east = (1, 0)
+    policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
+    agent = PassiveTDAgent(policy, sequential_decision_environment, alpha=lambda n: 60./(59+n))
+    for i in range(200):
+        run_single_trial(agent,sequential_decision_environment)
+    
+    agent.U[(0, 0)] > 0.2
     True
-    >>> agent.U[(0, 1)] > 0.2
+    agent.U[(0, 1)] > 0.2
     True
     """
 
@@ -227,22 +222,21 @@ class QLearningAgent:
     """ An exploratory Q-learning agent. It avoids having to learn the transition
         model because the Q-value of a state can be related directly to those of
         its neighbors. [Figure 21.8]
-    >>> import sys
-    >>> from mdp import sequential_decision_environment
-    >>> north = (0, 1)
-    >>> south = (0,-1)
-    >>> west = (-1, 0)
-    >>> east = (1, 0)
-    >>> policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
-    >>> sys.stdout.write('skip'); q_agent = QLearningAgent(sequential_decision_environment, Ne=5, Rplus=2, alpha=lambda n: 60./(59+n)) # doctest:+ELLIPSIS
-    skip ...
-    >>> sys.stdout.write('skip'); for i in range(200):
-    ...     run_single_trial(q_agent,sequential_decision_environment) # doctest:+ELLIPSIS
-    ... 
-    skip ...
-    >>> q_agent.Q[((0, 1), (0, 1))] >= -0.5
+
+    import sys
+    from mdp import sequential_decision_environment
+    north = (0, 1)
+    south = (0,-1)
+    west = (-1, 0)
+    east = (1, 0)
+    policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
+    q_agent = QLearningAgent(sequential_decision_environment, Ne=5, Rplus=2, alpha=lambda n: 60./(59+n))
+    for i in range(200):
+        run_single_trial(q_agent,sequential_decision_environment)
+    
+    q_agent.Q[((0, 1), (0, 1))] >= -0.5
     True
-    >>> q_agent.Q[((1, 0), (0, -1))] <= 0.5
+    q_agent.Q[((1, 0), (0, -1))] <= 0.5
     True
     """
     def __init__(self, mdp, Ne, Rplus, alpha=None):
