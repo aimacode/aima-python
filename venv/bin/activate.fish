9a6c9f2ebad3d530c7b67e69b66b0e9b0799f270
Justin Zemgulys <justinz@aloompa.com>
2018-08-28 17:25:54 -0500

    Assignment 1

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/oluwatitoebiwonjumi/Documents/aima-python/venv"
+set -gx VIRTUAL_ENV "/Users/Justin/aima-python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
