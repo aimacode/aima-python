c3e36e850385f93dde1272d70be6401befeb0266
Madison Everett <madison.everett@pop,belmont.edu>
2018-09-16 22:21:03 -0500

    ok

--- search.py
+++ search.py
@@ -1161,3 +1161,4 @@ def compare_graph_searchers():
                                 GraphProblem('Q', 'WA', australia_map)],
                       header=['Searcher', 'romania_map(Arad, Bucharest)',
                               'romania_map(Oradea, Neamt)', 'australia_map'])
+    
