3a833359cfaba5e7bbd3195976e76fda4b94030b
Nouman Ahmed <35970677+Noumanmufc1@users.noreply.github.com>
2018-09-20 03:33:26 +0500

    Added relu Activation (#960)
    * added relu activation
    
    * added default parameters

--- utils.py
+++ utils.py
@@ -273,7 +273,15 @@ def sigmoid(x):
     """Return activation value of x with sigmoid function"""
     return 1 / (1 + math.exp(-x))
 
+def relu(x):
+	return max(0, x)
 
+def relu_derivative(value):
+	if value > 0:
+		return 1
+	else:
+		return 0
+		
 def step(x):
     """Return activation value of x with sign function"""
     return 1 if x >= 0 else 0
