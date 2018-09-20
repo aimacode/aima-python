394bceda30af60590eafcbdefe757474b9459e2c
Enum81 <enumscratch81@gmail.com>
2018-09-19 21:31:01 -0500

    H6 Puzzle

--- submissions/Thompson/mySearches.py
+++ submissions/Thompson/mySearches.py
@@ -1,4 +1,5 @@
 import search
+import string
 from math import(cos, pi)
 
 # A sample map problem
@@ -93,11 +94,76 @@ class LightSwitch(search.Problem):
 switch_puzzle = LightSwitch('off')
 switch_puzzle.label = 'Light Switch'
 
+#===========================================================================================
+#===========================================================================================
+
+# class TrueOrFalse(search.Problem):
+#     def actions(self, state):
+#         return ['true', 'false']
+#
+#     def result(self, state, action):
+#         if action == 'true':
+#             return 'true'
+#         else:
+#             return 'false'
+#
+#     def goal_test(self, state):
+#         return state == 'true'
+#
+#     def h(self, node):
+#         state = node.state
+#         if self.goal_test(state):
+#             return 0
+#         else:
+#             return 1
+#
+# #swiss_puzzle = search.GraphProblem('A', 'Z', sumner_map)
+# trueorfalse_puzzle = TrueOrFalse('false')
+# trueorfalse_puzzle.label = 'True or False'
+
+cheese_map = search.UndirectedGraph(dict(
+    A1=dict(A2=10,A3=20,B1=10,B2=20,B3=30,C1=20,C2=30,C3=40),
+    A2=dict(A1=10,A3=10,B1=20,B2=10,B3=20,C1=30,C2=20,C3=30),
+    A3=dict(A1=20,A2=10,B1=30,B2=20,B3=10,C1=40,C2=30,C3=20),
+    B1=dict(A1=10,A2=20,A3=30,B2=10,B3=10,C1=10,C2=20,C3=30),
+    B2=dict(A2=10,A3=20,B1=10,A1=20,B3=10,C1=20,C2=10,C3=20),
+    B3=dict(A2=20,A3=10,B1=20,B2=10,A1=30,C1=30,C2=20,C3=10),
+    C1=dict(A2=20,A3=40,B1=10,B2=20,B3=30,A1=20,C2=10,C3=20),
+    C2=dict(A2=10,A3=20,B1=20,B2=10,B3=20,C1=10,A1=30,C3=10),
+    C3=dict(A2=30,A3=20,B1=30,B2=20,B3=10,C1=20,C2=10,A1=40),
+
+))
+
+import random
+def guess_letter():
+    return random.choice('ABC')
+
+def guess_number():
+    return random.choice('123')
+
+a = guess_letter()
+b = guess_number()
+
+print(a + b)
+
+
+cheese_puzzle = search.GraphProblem('A1', a+b , cheese_map)
+
+cheese_puzzle.label = 'Cheese Puzzle'
+
+#===========================================================================================
+#===========================================================================================
+
+
 mySearches = [
  #   swiss_puzzle,
  #   sumner_puzzle,
     romania_puzzle,
     switch_puzzle,
     norfolk_puzzle,
+    #trueorfalse_puzzle,
+    cheese_puzzle,
+
+
 ]
 
