568e1cbabcf6feae9810bb3dda1677d86e96ae95
WmHHooper <william.hooper@belmont.edu>
2018-09-20 21:26:23 -0500

    The revised template contains a basic set of classes and methods. The comments attached to the template are clearer. Another game, ThinkAhead, is implemented in submissions/cccute.

--- submissions/aardvark/mygames.py
+++ submissions/aardvark/mygames.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 from games import (Game)
+from queue import PriorityQueue
+from copy import deepcopy
 
 class GameState:
     def __init__(self, to_move, board, label=None, depth=8):
@@ -178,36 +180,68 @@ lost = GameState(
     label = 'lost'
 )
 
-class EmptyState:
-    def __init__(self, junk):
-        self.to_move = junk
+class TemplateState:    # one way to define the state of a minimal game.
 
-class Empty(Game):
-    """The minimal coplining game."""
+    def __init__(self, player): # add parameters as needed.
+        self.to_move = player
+        self.label = str(id(self))   # change this to something easier to read
+        # add code and self.variables as needed.
 
-    def __init__(self, initial):
+    def __str__(self):  # use this exact signature
+        return self.label
+
+# class TemplateAction:
+#     '''
+#     It is not necessary to define an action.
+#     Start with actions as simple as a label (e.g., 'Down')
+#     or a pair of coordinates (e.g., (1,2)).
+#
+#     Don't un-comment this until you already have a working game,
+#     and want to play smarter.
+#     '''
+#     def __lt__(self, other):    # use this exact signature
+#         # return True when self is a better move than other.
+#         return False
+
+class TemplateGame(Game):
+    '''
+    This is a minimal Game definition,
+    the shortest implementation I could run without errors.
+    '''
+
+    def __init__(self, initial):    # add parameters if needed.
         self.initial = initial
+        # add code and self.variables if needed.
 
-    def actions(self, state):
-        return [ '!' ]
+    def actions(self, state):   # use this exact signature.
+        acts = []
+        # append all moves, which are legal in this state,
+        # to the list of acts.
+        return acts
 
-    # defines the order of play
-    def opponent(self, player):
-        return None
+    def result(self, state, move):   # use this exact signature.
+        newState = deepcopy(state)
+        # use the move to modify the newState
+        return newState
 
-    def result(self, state, move):
-        return None
-
-    def terminal_test(self, state):
+    def terminal_test(self, state):   # use this exact signature.
+        # return True only when the state of the game is over.
         return True
 
-    def utility(self, state, player):
+    def utility(self, state, player):   # use this exact signature.
+        ''' return:
+        >0 if the player is winning,
+        <0 if the player is losing,
+         0 if the state is a tie.
+        '''
         return 0
 
-    def display(self, state):
-        print('?')
+    def display(self, state):   # use this exact signature.
+        # pretty-print the game state, using ASCII art,
+        # to help a human player understand his options.
+        print(state)
 
-eg = Empty(EmptyState('A'))
+tg = TemplateGame(TemplateState('A'))   # this is the game we play interactively.
 
 myGames = {
     myGame: [
@@ -216,7 +250,9 @@ myGames = {
         lost,
     ],
 
-    eg: [
-        EmptyState('B'),
+    tg: [
+        # these are the states we tabulate when we test AB(1), AB(2), etc.
+        TemplateState('B'),
+        TemplateState('C'),
     ]
 }
