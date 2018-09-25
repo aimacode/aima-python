26d9786ec2184c1b40f38ec09a99ce748d1b8f9a
Madison Everett <madison.everett@pop,belmont.edu>
2018-09-24 22:35:16 -0500

    I think this progress...

--- submissions/Everett/mygames.py
+++ submissions/Everett/mygames.py
@@ -1,153 +1,156 @@
 from collections import namedtuple
 from games import (Game)
-from copy import deepcopy
-from utils import isnumber
-from math import nan, isnan
-from queue import PriorityQueue
-from utils import isnumber
-from grading.util import print_table
 
-# Make an list of words
-# print at the beginning
-#easy 4 letter words
-#medium 5 letter words
-#hard 8 letter words
-#Impossible combine all the lists and have 10 letter words
-
-#print letters each time
-# Check if letter is in the alphabet
-# If not input again
-# each turn iterate through the word list
-# if there is a word that exists in one of the turns, the last player who played loses
-# if there is a set of letters that exist that doesn't lead to any other word within the list
-# the last player loses.
-# Start with a veryyyy small list.
-
-# dictionary class with - check alpha, list, check if word, check if guessing
-#for different difculties
-#actions, a-z
-
-lowerletters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z']
-upperletters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-#
-class GhostState:
-    def __init__(self, to_move, board, Uletters, Lletters, label):
+class GameState:
+    def __init__(self, to_move, board, label=None, depth=8):
         self.to_move = to_move
+        self.board = board
         self.label = label
-        self.board = []
-        self.Uletters = Uletters
-        self.Lletters = Lletters
+        self.maxDepth = depth
 
     def __str__(self):
         if self.label == None:
-            return super(GhostState, self).__str__()
+            return super(GameState, self).__str__()
         return self.label
 
+class Gomoku(Game):
+    """ The Japanese strategy based game of five in a row. It is orignally played on a 19x19 board,
+    however, we don't have the memory for that. So it made a baby version of it that is 5x5"""
 
-class Ghost(Game):
-    """A word game that avoids the inevitable."""
-    def __init__(self, state):
-        self.initial = state
+    def __init__(self, l=5, w=5, r=5):
+        self.l = l
+        self.w = w
+        self.r = r
+        self.initial = GameState(to_move='B', board={})
 
     def actions(self, state):
-        #if state.to_move == 'U':
-           # moves = upperletters
-         #   return moves
-        #if state.to_move == 'L':
-         #   moves = lowerletters
-          #  return moves
-        return upperletters
+        try:
+            return state.moves
+        except:
+            pass
+        "Legal moves are any square not yet taken."
+        moves = []
+        for x in range(1, self.l + 1):
+            for y in range(1, self.w + 1):
+                if (x,y) not in state.board.keys():
+                    moves.append((x,y))
+        state.moves = moves
+        return moves
 
+    # defines the order of play
     def opponent(self, player):
-        if player == 'U':
-            return 'L'
-        if player == 'U':
-            return 'L'
+        if player == 'B':
+            return 'W'
+        if player == 'W':
+            return 'B'
+        return None
 
     def result(self, state, move):
         if move not in self.actions(state):
-           return state
-        currMover = state.to_move
-        nextMover = self.opponent(currMover)
-        newState = deepcopy(state)
-        return newState
-
+            return state  # Illegal move has no effect
+        board = state.board.copy()
+        player = state.to_move
+        board[move] = player
+        next_mover = self.opponent(player)
+        return GameState(to_move=next_mover, board=board)
 
     def utility(self, state, player):
-       opponent = self.opponent(player)
+        "Return the value to player; 1 for win, -1 for loss, 0 otherwise."
+        try:
+            return state.utility if player == 'B' else -state.utility
+        except:
+            pass
+        board = state.board
+        util = self.check_win(board, 'B')
+        if util == 0:
+            util = -self.check_win(board, 'W')
+        state.utility = util
+        return util if player == 'B' else -util
 
-       return 1
+    # Did I win?
+    def check_win(self, board, player):
+        # check rows
+        for y in range(1, self.w + 1):
+            if self.r_in_row(board, (1,y), player, (1,0)):
+                return 1
+        # check columns
+        for x in range(1, self.l + 1):
+            if self.r_in_row(board, (x,1), player, (0,1)):
+                return 1
+        # check \ diagonal
+        if self.r_in_row(board, (1,1), player, (1,1)):
+            return 1
+        # check / diagonal
+        if self.r_in_row(board, (5,1), player, (-1,1)):
+            return 1
+        return 0
 
+    # does player have K in a row? return 1 if so, 0 if not
+    def r_in_row(self, board, start, player, direction):
+        "Return true if there is a line through start on board for player."
+        (delta_x, delta_y) = direction
+        x, y = start
+        n = 0  # n is number of moves in row
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x + delta_x, y + delta_y
+        x, y = start
+        while board.get((x, y)) == player:
+            n += 1
+            x, y = x - delta_x, y - delta_y
+        n -= 1  # Because we counted start itself twice
+        return n >= self.r
 
-    def cheating(bob, turn):
-        Ghostwords = ['chair', 'house', 'lamp']
-        for x in Ghostwords:
-            if x[0: turn] != bob:
-                return True
-            else:
-                return False
-
-    def checkifword(self, player, word):
-        Ghostwords = ['chair', 'house', 'lamp']
-        if self.checkcase(player) == 1:
-            for x in Ghostwords:
-                if word.lower() == x:
-                    return 3
-                else:
-                    return False
-
-        if self.checkcase(player) == 0:
-            for x in Ghostwords:
-                if word.lower() == x:
-                    return 5
-                else:
-                    return False
-        pass
-
-    def changeboard(thing, board):
-       return board.append(thing.islower())
-
-    def terminal_test(self, board):
-     # return self.checkifword(board) == True
-       return None
+    def terminal_test(self, state):
+        "A state is terminal if it is won or there are no empty squares."
+        return self.utility(state, 'B') != 0 or len(self.actions(state)) == 0
 
     def display(self, state):
-        print("Hello and welcome to a game called Ghost. Ghost is a game where the point is to not finish a word." + '\n'
-        "The rules are: No made up words, Proper nouns, or Names." + '\n' 
-        "Here is the wordlist for this round:" + '\n')
-        Ghostwords = ['chair', 'house', 'lamp']
-        for x in Ghostwords:
-            bob = x
-            print(bob)
-        print(state)
+        board = state.board
+        for x in range(1, self.l + 1):
+            for y in range(1, self.w + 1):
+                print(board.get((x, y), '.'), end=' ')
+            print()
 
-        print('Letters: ' + str(state.letter))
+myGame = Gomoku()
 
-lose = GhostState(
-    to_move = 'U',
-    board = ['c','h','a','i','r'],
-    label = 'Lose',
-    Uletters=['c','a','r'],
-    Lletters=['h','i']
+won = GameState(
+    to_move = 'B',
+    board = {(1,1): 'B', (1,2): 'B', (1,3): 'B', (1,4): 'B', (1,5): 'B',
+             (2,1): 'W', (2,2): 'W', (2,3): 'W', (2,4): 'W'
+            },
+    label = 'won'
+)
+won2 = GameState(
+    to_move = 'B',
+    board = {(1,1): 'B', (2,1): 'B', (3,1): 'B', (4,1): 'B', (5,1): 'B',
+             (2,4): 'W', (2,2): 'W', (2,3): 'W', (2,4): 'W'
+            },
+    label = 'won'
 )
 
-
-win = GhostState(
-    to_move = 'L',
-    board = ['l','a','m','p'],
-    label = 'Win',
-    Uletters=['l', 'm'],
-    Lletters=['a', 'p']
+lost = GameState(
+    to_move = 'B',
+    board = {(1,1): 'B', (1,2): 'B', (1,3): 'B', (1,4): 'B',
+             (2,1): 'W', (2,2): 'W', (2,3): 'W', (2,4): 'W', (2,5): 'W',
+             (3,1): 'B'
+            },
+    label = 'lost'
 )
 
-
-
-ghost1 = Ghost(lose)
-
+win1 = GameState(
+    to_move = 'B',
+    board = {(3,1): 'B', (3,2): 'B', (3,3): 'B', (3,4): 'B',
+             (2,1): 'W', (2,2): 'W', (2,3): 'W',
+            },
+    label = 'win by 1'
+)
 myGames = {
+    myGame: [
+        won,
+        lost,
+       # win1,
+        won2
+    ],
 
-    ghost1: [
-
-        lose,
-    ]
 }
