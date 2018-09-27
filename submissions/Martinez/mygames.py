ea8ed58a8b1526661fa6b6c8c786db69657d0b78
omarmartinez97 <omar.martinez@pop.belmont.edu>
2018-09-27 09:13:24 -0500

    Add files via upload

--- submissions/Martinez/mygames.py
+++ submissions/Martinez/mygames.py
@@ -58,7 +58,7 @@ def movesInCol(board, c):
         mQueue.put(move)
     return q2list(mQueue)
 
-class ThinkAhead(Game):
+class Hame(Game):
     """
     An implementation of ThinkAhead
     """
@@ -273,11 +273,146 @@ winby10 = GameState(
 # print("Ship 2 is hidden:")
 # print(ship_row_2)
 # print(ship_col_2)
+class Hame(Game):
+    """
+    An implementation of ThinkAhead
+    """
+    def __init__(self, state):
+        self.initial = state
 
-thinkA = ThinkAhead(stolen)
+    def actions(self, state):
+        "Legal moves are any square not yet taken."
+        r, c = state.position
+        if state.to_move == 'H':
+            moves = movesInRow(state.board, r)
+            return moves
+        if state.to_move == 'V':
+            moves = movesInCol(state.board, c)
+            return moves
+        return []
+
+    # defines the order of play
+    def opponent(self, player):
+        if player == 'H':
+            return 'V'
+        if player == 'V':
+            return 'H'
+        return None
+
+    def result(self, state, move):
+        r, c, v = move
+        assert state.board[r][c] == v
+        currMover = state.to_move
+        nextMover = self.opponent(currMover)
+
+        newState = deepcopy(state)
+        newState.to_move = nextMover
+        newState.position = r, c
+        newState.board[r][c] = nan
+        newState.scores[currMover] += v
+        return newState
+
+    def utility(self, state, player):
+        "Player relative score"
+        opponent = self.opponent(player)
+        return state.scores[player] - state.scores[opponent]
+
+    def terminal_test(self, state):
+        "A state is terminal if it is won or there are no empty squares."
+        return len(self.actions(state)) == 0
+
+    def display(self, state):
+        print_table(state.board, njust='center', sep=',')
+
+        print('Score: ' + str(state.scores))
+
+won = GameState(
+    to_move = 'H',
+    position = (0,1),
+    board = [[nan,nan],
+             [ 9 ,nan]],
+    label = 'won'
+)
+won.scores = {'H':9, 'V': 0}
+
+lost = GameState(
+    to_move = 'V',
+    position = (0,1),
+    board = [[nan,nan],
+             [ 9 ,nan]],
+    label = 'lost'
+)
+lost.scores = {'H':0, 'V': 9}
+
+winin1 = GameState(
+    to_move = 'H',
+    position = (1,1),
+    board = [[nan,nan],
+             [ 9 ,nan]],
+    label = 'winin1'
+)
+
+losein1 = GameState(
+    to_move = 'V',
+    position = (0,0),
+    board = [[nan,nan],
+             [ 9 ,nan]],
+    label = 'losein1'
+)
+
+winin2 = GameState(
+    to_move = 'H',
+    position = (0,0),
+    board = [[nan, 3 , 2 ],
+             [nan, 9 ,nan],
+             [nan,nan, 1]],
+    label = 'winin2'
+)
+
+losein2 = GameState(
+    to_move = 'V',
+    position = (0,0),
+    board = [[nan,nan,nan],
+             [ 3 , 9 ,nan],
+             [ 2 ,nan, 1]],
+    label = 'losein2'
+)
+losein2.maxDepth = 3
+
+stolen = GameState(
+    to_move = 'H',
+    position = (3,1),
+    board = [[3, 8 ,9,5],
+             [9, 1 ,3,2],
+             [8, 6 ,4,4],
+             [9,nan,1,5]],
+    label = 'stolen'
+)
+
+choose1 = GameState(
+    to_move = 'H',
+    position = (1,0),
+    board = [[ 3 , 8 ,9,5],
+             [nan, 1 ,3,2],
+             [ 8 , 6 ,4,4],
+             [nan,nan,1,5]],
+    label = 'choose1'
+)
+
+winby10 = GameState(
+    to_move='H',
+    position=(2, 0),
+    board=[[nan, nan, nan, nan],
+           [nan, nan, nan, nan],
+           [nan,  6 ,  4 ,  5 ],
+           [nan, nan,  1 ,  3 ]],
+    label = 'winby10'
+)
+
+Game1 = Hame
 
 myGames = {
-    thinkA: [
+    Game1: [
         won,
         lost,
         winin1,
