1fa8bf8f5468404ca905a04df5858e763c39bd17
Madison Everett <madison.everett@pop,belmont.edu>
2018-10-06 15:35:01 -0500

    I think this progress...

--- submissions/Everett/myCSPs.py
+++ submissions/Everett/myCSPs.py
@@ -1,22 +1,10 @@
 import csp
 
-rgb = ['R', 'G', 'B','P','T']
+rgb = ['R', 'G', 'B', 'P']
 
 # The 22 wards of Tokyo
-domains = {
-    'ADACHI': rgb,
-    'KATSU':  rgb,
-    'EDOG':   rgb,
-    'KOTO':   rgb,
-    'CHUO':   rgb,
-    'SUMI':   rgb,
-    'TAITO':  rgb,
-    'ARAK':   rgb,
-    'KITA':   rgb,
-    'BUNK':   rgb,
-    'CHIY':   rgb,
-    'MINA':   rgb,
-    'SHIN':   rgb,
+
+d2 = {'ADACHI' : rgb, 'KATSU' : rgb, 'EDOG' : rgb, 'KOTO' : rgb, 'CHUO': rgb, 'SUMI': rgb, 'TAITO':  rgb,    'ARAK':   rgb, 'KITA':   rgb, 'BUNK':   rgb, 'CHIY':   rgb, 'MINA':   rgb, 'SHIN':   rgb,
     'SHINA':  rgb,
     'OTA':    rgb,
     'MEGU':   rgb,
@@ -27,38 +15,33 @@ domains = {
     'ITA':    rgb,
     'TOSHI':  rgb,
     'SHIB' :  rgb,
+ }
 
-}
+v2 = d2.keys()
 
-variables = domains.keys()
-
-neighbors = {
-    'ADACHI': ['KATSU','SUMI','ARAK','KITA'],
-    'KATSU': ['ADACHI','SUMI','EDOG'],
-    'EDOG':  ['KATSU','SUMI','KOTO'],
-    'KOTO': ['SUMI','EDOG','CHUO'],
-    'CHUO': ['MINA','KOTO','CHIY','SUMI','TAITO'],
-    'SUMI': ['KATSU','EDOG','KOTO','CHUO','TAITO','ARAK','ADACHI'],
-    'TAITO': ['CHUO','SUMI','CHIY','BUNK','KITA','ARAK'],
-    'ARAK': ['TAITO','BUNK','SUMI','ADACHI','KITA'],
-    'KITA':  ['ADACHI','ARAK','BUNK','TOSHI','ITA'],
-    'BUNK': ['ARAK','TAITO','CHIY','SHIN','TOSHI','KITA'],
-    'CHIY': ['CHUO','MINA','SHIN','BUNK','TAITO'],
-    'MINA': ['CHUO','CHIY','SHIN','SHIB','SHINA'],
-    'SHIN': ['MINA','CHIY','BUNK','TOSHI','NERI','NAKA','SHIB'],
-    'SHINA': ['MINA','SHIB','MEGU','OTA'],
-    'OTA': ['SHINA','MEGU','SETA'],
-    'MEGU': ['SHIB','MINA','SHINA','OTA','SETA',],
-    'SETA': ['OTA','MEGU','SHIB','SUGI'],
-    'SUGI': ['SETA','NERI','NAKA','SHIB'],
-    'NAKA': ['SUGI','SHIB','SHIN','TOSHI','NERI'],
-    'NERI': ['TOSHI','NAKA','SUGI','SHIN','ITA'],
-    'ITA': ['KITA','NERI','TOSHI'],
-    'TOSHI': ['KITA','BUNK','ITA','NERI','NAKA','SHIN'],
-    'SHIB': ['NAKA','SHIN','SUGI','SETA','MEGU','SHINA','MINA'],
-
-}
-
+n2 = {'ADACHI' : ['KATSU','SUMI','ARAK','KITA'],
+      'KATSU' : ['ADACHI', 'EDOG','SUMI'],
+      'EDOG' : ['KATSU', 'KOTO','SUMI'],
+      'KOTO' : ['EDOG','CHUO','SUMI'],
+      'CHUO':['KOTO','SUMI','MINA','CHIY','TAITO'],
+      'TAITO':['CHUO','SUMI','CHIY','BUNK','KITA','ARAK'],
+      'SUMI':['KATSU','EDOG','KOTO','CHUO','TAITO','ADACHI','ARAK'],
+      'ARAK': ['TAITO', 'BUNK', 'SUMI', 'ADACHI', 'KITA'],
+      'KITA': ['ADACHI', 'ARAK', 'BUNK', 'TOSHI', 'ITA','TAITO'],
+      'BUNK': ['ARAK', 'TAITO', 'CHIY', 'SHIN', 'TOSHI', 'KITA'],
+      'CHIY': ['CHUO', 'MINA', 'SHIN', 'BUNK', 'TAITO'],
+      'MINA': ['CHUO', 'CHIY', 'SHIN', 'SHIB', 'SHINA'],
+      'SHIN': ['MINA', 'CHIY', 'BUNK', 'TOSHI', 'NERI', 'NAKA', 'SHIB'],
+      'SHINA': ['MINA', 'SHIB', 'MEGU', 'OTA'],
+      'OTA': ['SHINA', 'MEGU', 'SETA'],
+      'MEGU': ['SHIB', 'MINA', 'SHINA', 'OTA', 'SETA', ],
+      'SETA': ['OTA', 'MEGU', 'SHIB', 'SUGI'],
+      'SUGI': ['SETA', 'NERI', 'NAKA', 'SHIB'],
+      'NAKA': ['SUGI', 'SHIB', 'SHIN', 'TOSHI', 'NERI'],
+      'NERI': ['TOSHI', 'NAKA', 'SUGI', 'SHIN', 'ITA'],
+      'ITA': ['KITA', 'NERI', 'TOSHI'],
+      'TOSHI': ['KITA', 'BUNK', 'ITA', 'NERI', 'NAKA', 'SHIN'],
+      'SHIB': ['NAKA', 'SHIN', 'SUGI', 'SETA', 'MEGU', 'SHINA', 'MINA'],}
 
 def constraints(A, a, B, b):
     if A == B:      # e.g. NSW == NSW
@@ -69,15 +52,17 @@ def constraints(A, a, B, b):
 
     return True
 
-
-myJapan = csp.CSP(variables, domains, neighbors, constraints)
-#bob = csp.mrv(5, myJapan)
-#inference = csp.mac()
-#okiedokie=csp.forward_checking()
+c2 = csp.CSP(v2, d2, n2, constraints)
+c2.label = 'Map of Japan'
 
 myCSPs = [
-    {'csp': myJapan,
-     # 'select_unassigned_variable':csp.mrv,
-    # 'heurisic': bob,
-     }
+    {
+        'csp' : c2,
+        # 'select_unassigned_variable': csp.mrv,
+        # 'order_domain_values': csp.lcv,
+        # 'inference': csp.mac,
+        # 'inference': csp.forward_checking,
+    },
+
+
 ]
