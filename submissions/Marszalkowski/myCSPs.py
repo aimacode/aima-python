38e05bfcb89f9bc9b4713a1fa1aed77151668401
Johnmarsz <marsz2soccer@yahoo.com>
2018-10-10 17:15:02 -0500

    CSP homework complete version

--- submissions/Marszalkowski/myCSPs.py
+++ submissions/Marszalkowski/myCSPs.py
@@ -13,21 +13,23 @@ domains = {
     'T': rgby,
     'GHC': rgby,
     'M': rgby,
+    'TH': rgby
 }
 
 variables = domains.keys()
 
 neighbors = {
+    'TH': ['BW', 'BB', 'RB', 'H'],
     'GF': ['BW', 'BB'],
-    'BW': ['GF', 'BB', 'RB', 'H'],
-    'BB': ['GF', 'BW', 'H', 'BF'],
-    'RB': ['BW', 'H', 'T', 'MD'],
-    'MD': ['RB', 'T'],
-    'H': ['RB', 'BW', 'BB', 'BF', 'GHC', 'T'],
+    'BW': ['GF', 'BB', 'RB', 'H', 'TH'],
+    'BB': ['GF', 'BW', 'H', 'BF', 'TH'],
+    'RB': ['BW', 'H', 'T', 'MD', 'TH'],
+    'MD': ['RB', 'T', 'GHC'],
+    'H': ['RB', 'BW', 'BB', 'BF', 'GHC', 'T', 'TH'],
     'BF': ['BB', 'H', 'GHC', 'M'],
     'T': ['MD', 'RB', 'H', 'GHC'],
-    'GHC': ['T', 'H', 'BF', 'M'],
-    'M': ['GHC', 'BF']
+    'GHC': ['T', 'H', 'BF', 'M', 'MD'],
+    'M': ['GHC', 'BF'],
 }
 
 def constraints(A, a, B, b):
@@ -39,10 +41,51 @@ def constraints(A, a, B, b):
 
     return True
 
-ShireCSP = csp.CSP(variables, domains, neighbors, constraints)
+
+shireCSP = csp.CSP(variables, domains, neighbors, constraints)
+shireCSP.label = 'Shire Map'
 
 myCSPs = [
-    {'csp': ShireCSP,
-     'select_unassigned_variable':csp.mrv,
-     }
+    {
+        'csp': shireCSP,
+        # 'select_unassigned_variable': csp.mrv,
+        # 'order_domain_values': csp.lcv,
+        # 'inference': csp.mac,
+        # 'inference': csp.forward_checking,
+    },
+    {
+        'csp': shireCSP,
+        'select_unassigned_variable': csp.mrv,
+        # 'order_domain_values': csp.lcv,
+        # 'inference': csp.mac,
+        # 'inference': csp.forward_checking,
+    },
+    {
+        'csp': shireCSP,
+        # 'select_unassigned_variable': csp.mrv,
+        'order_domain_values': csp.lcv,
+        # 'inference': csp.mac,
+        # 'inference': csp.forward_checking,
+    },
+    {
+        'csp': shireCSP,
+        # 'select_unassigned_variable': csp.mrv,
+        # 'order_domain_values': csp.lcv,
+        'inference': csp.mac,
+        # 'inference': csp.forward_checking,
+    },
+    {
+        'csp': shireCSP,
+        # 'select_unassigned_variable': csp.mrv,
+        # 'order_domain_values': csp.lcv,
+        # 'inference': csp.mac,
+        'inference': csp.forward_checking,
+    },
+    {
+        'csp': shireCSP,
+        'select_unassigned_variable': csp.mrv,
+        'order_domain_values': csp.lcv,
+        'inference': csp.mac,
+        # 'inference': csp.forward_checking,
+    },
 ]
