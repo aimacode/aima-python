7624b6afd137a88411c7fb599fb8f607947b5de3
Madison Everett <madison.everett@pop,belmont.edu>
2018-10-18 20:21:45 -0500

    I think this progress...

--- submissions/Everett/myLogic.py
+++ submissions/Everett/myLogic.py
@@ -1,25 +1,44 @@
 family = {
     'kb': '''
-Girl(Sandy)
+Daughter(Sandy)
+Daughter(Dale)
+Cousin(Jasmine)
+Daughter(Madison)
+Daughter(Megan)
+Daughter(Morgan)
+Mother(Mattie, Sandy)
+Mother(Mattie,Dale)
+Mother(Dale,Jasmine)
 Mother(Sandy,Madison)
+Mother(Sandy,Morgan)
+Mother(Sandy,Megan)
+(Mother(x,y) & Daughter(y)) ==> Sister(y)
+(Sister(y) & Daughter(y)) ==> Girl(y)
+(Mother(y,z)) ==> Girl(y)
+(Mother(y,z) & Sister(y) & Girl(y)) ==> Aunt(y)
+
+
 
 ''',
 
 'queries':'''
 
+Sister(y)
+Girl(x)
+Daughter(x)
+Aunt(x)
+
 ''',
      'Differ': '''
-
-
-
+"Sandy", "Dale"
 
    ''',
 
-      'limit': '''
-  
-  
-  
-  '''
+
+
+'limit': 4,
+
+
 }
 
 Examples = {
