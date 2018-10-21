e82e79af399faf569680e2dd497aec3f29ede679
Oluwatito Ebiwonjumi <titoebiwonjumi@gmail.com>
2018-10-21 17:43:04 -0500

    70 point submission

---
+++ submissions/Ebiwonjumi/myLogic.py
@@ -0,0 +1,61 @@
+farmer = {
+    'kb': '''
+Farmer(Mac)
+Rabbit(Pete)
+Mother(MrsMac, Mac)
+Mother(MrsRabbit, Pete)
+(Rabbit(r) & Farmer(f)) ==> Hates(f, r)
+(Mother(m, c)) ==> Loves(m, c)
+(Mother(m, r) & Rabbit(r)) ==> Rabbit(m)
+(Farmer(f)) ==> Human(f)
+(Mother(m, h) & Human(h)) ==> Human(m)
+''',
+# Note that this order of conjuncts
+# would result in infinite recursion:
+# '(Human(h) & Mother(m, h)) ==> Human(m)'
+    'queries':'''
+Human(x)
+Hates(x, y)
+''',
+#    'limit': 1,
+}
+
+weapons = {
+    'kb': '''
+(American(x) & Weapon(y) & Sells(x, y, z) & Hostile(z)) ==> Criminal(x)
+Owns(Nono, M1)
+Missile(M1)
+(Missile(x) & Owns(Nono, x)) ==> Sells(West, x, Nono)
+Missile(x) ==> Weapon(x)
+Enemy(x, America) ==> Hostile(x)
+American(West)
+Enemy(Nono, America)
+''',
+    'queries':'''
+Criminal(x)
+''',
+}
+
+nigerians = {
+      'kb': '''
+Igbo(Chukwu)
+Yoruba(Ayo)
+Hausa(Suleiman)
+ (Igbo(i)) ==> Rich(i)
+ (Hausa(h)) ==> Farmer(h)
+ (Yoruba(y)) ==> Smart(y)
+ (Hausa(h) & Yoruba(y)) ==> Friends(h, y)
+ 
+''',
+    'queries':'''
+Rich(x)
+Friends(x, y)
+''',
+
+}
+
+Examples = {
+   # 'farmer': farmer,
+   # 'weapons': weapons,
+   'nigerians': nigerians,
+}
