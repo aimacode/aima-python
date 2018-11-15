61e742de1a55aa6448e68c82d08a9480373f8186
Justin Zemgulys <justinz@aloompa.com>
2018-11-14 18:29:19 -0600

    85 point submission

--- submissions/Zemgulys/myNN.py
+++ submissions/Zemgulys/myNN.py
@@ -22,10 +22,17 @@ fashion_mnist = keras.datasets.fashion_mnist
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-train_labels = np.reshape(train_labels, (train_labels.shape[0], 1))
-test_labels = np.reshape(test_labels, (test_labels.shape[0], 1))
+train_labels=keras.utils.to_categorical(train_labels,10)
+test_labels=keras.utils.to_categorical(test_labels,10)
+
+# train_labels = np.reshape(train_labels, (train_labels.shape[0], 1))
+# test_labels = np.reshape(test_labels, (test_labels.shape[0], 1))
 
 model = Sequential()
+model.add(keras.layers.Conv2D(input_shape=(train_images.shape[0],train_images.shape[1],train_images.shape[2]),filters=32,kernel_size=(3,3),strides=(1,1),activation='relu',padding='same'))
+model.add(keras.layers.Conv2D(filters=32,kernel_size=(3,3),strides=(2,2),activation='relu'))
+model.add(keras.layers.Conv2D(filters=64,kernel_size=(5,5),strides=(2,2),activation='relu'))
+model.add(keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2,2)))
 model.add(keras.layers.Flatten())
 model.add(Dense(128, activation=tf.nn.relu))
 model.add(Dense(10, activation=tf.nn.softmax))
@@ -34,7 +41,7 @@ model.compile(optimizer=tf.train.AdamOptimizer(),
               loss=keras.losses.sparse_categorical_crossentropy,
               metrics=[keras.metrics.sparse_top_k_categorical_accuracy])
 
-model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
+model.fit(train_images, train_labels, batch_size=round(train_images.shape[0]/5), epochs=5, validation_data=(test_images, test_labels))
 myWeights = model.get_weights()
 np.set_printoptions(suppress=True)
 np.set_printoptions(precision=2)
