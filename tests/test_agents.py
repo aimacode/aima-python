752adeed6aab0f6f96a2226f9ed15853f216d327
Rajat Jain <1997.rajatjain@gmail.com>
2019-02-14 17:51:47 +0530

    Added WumpusWorld testcases
    Added:
     - Testcases in test_agents.py
    Modified:
     - Duplicate walls are not added in corners now

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -4,6 +4,7 @@ from agents import Agent
 from agents import ReflexVacuumAgent, ModelBasedVacuumAgent, TrivialVacuumEnvironment, compare_agents,\
                    RandomVacuumAgent, TableDrivenVacuumAgent, TableDrivenAgentProgram, RandomAgentProgram, \
 		           SimpleReflexAgentProgram, ModelBasedReflexAgentProgram, rule_match
+from agents import Wall, Gold, Explorer, Thing, Bump, Glitter, WumpusEnvironment, Pit
 
 
 random.seed("aima-python")
@@ -264,3 +265,54 @@ def test_Agent():
     agent = Agent(constant_prog)
     result = agent.program(5)
     assert result == 5
+
+def test_WumpusEnvironment():
+    def constant_prog(percept):
+        return percept
+    # Initialize Wumpus Environment
+    w = WumpusEnvironment(constant_prog)
+
+    #Check if things are added properly
+    assert len([x for x in w.things if isinstance(x, Wall)]) == 20
+    assert any(map(lambda x: isinstance(x, Gold), w.things))
+    assert any(map(lambda x: isinstance(x, Explorer), w.things))
+    assert not any(map(lambda x: not isinstance(x,Thing), w.things))
+
+    #Check that gold and wumpus are not present on (1,1)
+    assert not any(map(lambda x: isinstance(x, Gold) or isinstance(x,WumpusEnvironment), 
+                    w.list_things_at((1,1)))) 
+
+    #Check if w.get_world() segments objects correctly
+    assert len(w.get_world()) == 6
+    for row in w.get_world():
+        assert len(row) == 6
+
+    #Start the game!
+    agent = [x for x in w.things if isinstance(x, Explorer)][0]
+    gold = [x for x in w.things if isinstance(x, Gold)][0]
+    pit = [x for x in w.things if isinstance(x, Pit)][0]
+
+    assert w.is_done()==False
+
+    #Check Walls
+    agent.location = (1,2)
+    percepts = w.percept(agent)
+    assert len(percepts) == 5
+    assert any(map(lambda x: isinstance(x,Bump), percepts[0]))
+
+    #Check Gold
+    agent.location = gold.location
+    percepts = w.percept(agent)
+    assert any(map(lambda x: isinstance(x,Glitter), percepts[4]))
+    agent.location = (gold.location[0], gold.location[1]+1)
+    percepts = w.percept(agent)
+    assert not any(map(lambda x: isinstance(x,Glitter), percepts[4]))
+
+    #Check agent death
+    agent.location = pit.location
+    assert w.in_danger(agent) == True
+    assert agent.alive == False
+    assert agent.killed_by == Pit.__name__
+    assert agent.performance == -1000
+
+    assert w.is_done()==True
