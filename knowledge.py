6353b09fdffd2e1fd1886f98ec871b69c9637b4a
sagar-sehgal <sagar.r16@iiits.in>
2019-03-03 13:34:57 +0530

    some optimizations in knowledge.py

--- knowledge.py
+++ knowledge.py
@@ -12,14 +12,13 @@ from functools import partial
 # ______________________________________________________________________________
 
 
-def current_best_learning(examples, h, examples_so_far=None):
+def current_best_learning(examples, h, examples_so_far=[]):
     """ [Figure 19.2]
     The hypothesis is a list of dictionaries, with each dictionary representing
     a disjunction."""
     if not examples:
         return h
 
-    examples_so_far = examples_so_far or []
     e = examples[0]
     if is_consistent(e, h):
         return current_best_learning(examples[1:], h, examples_so_far + [e])
