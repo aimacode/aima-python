fc918001668d96875ef915c4a8cd8569fd39016c
shivam pandey <geetapandey595@gmail.com>
2019-03-31 22:03:50 +0530

    code updated and fixed

--- single_layer_Auto_encoder.py
+++ single_layer_Auto_encoder.py
@@ -34,8 +34,8 @@ x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
 x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
 x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-print x_train.shape
-print x_test.shape
+print (x_train.shape)
+print (x_test.shape)
 
 autoencoder.fit(x_train, x_train,
                 epochs=50,
