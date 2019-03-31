8481be732ad6addfe88d7c818b8b9f62c7f92220
shivam pandey <geetapandey595@gmail.com>
2019-03-31 15:36:46 +0530

    single_layer_Auto_encoder.py
    here is the representation by using external libraries(keras) of a single layer Auto encoder.

---
+++ single_layer_Auto_encoder.py
@@ -0,0 +1,84 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+
+# this is the size of our encoded representations
+encoding_dim = 32  # 32 floats -> compression of factor 24.5, assuming the input is 784 floats
+
+# this is our input placeholder
+input_img = Input(shape=(784,))
+# "encoded" is the encoded representation of the input
+encoded = Dense(encoding_dim, activation='relu')(input_img)
+# "decoded" is the lossy reconstruction of the input
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+# this model maps an input to its reconstruction
+autoencoder = Model(input_img, decoded)
+
+# this model maps an input to its encoded representation
+encoder = Model(input_img, encoded)
+
+# create a placeholder for an encoded (32-dimensional) input
+encoded_input = Input(shape=(encoding_dim,))
+# retrieve the last layer of the autoencoder model
+decoder_layer = autoencoder.layers[-1]
+# create the decoder model
+decoder = Model(encoded_input, decoder_layer(encoded_input))
+
+autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+
+from keras.datasets import mnist
+import numpy as np
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+print x_train.shape
+print x_test.shape
+
+autoencoder.fit(x_train, x_train,
+                epochs=50,
+                batch_size=256,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+                
+# encode and decode some digits
+# note that we take them from the *test* set
+encoded_imgs = encoder.predict(x_test)
+decoded_imgs = decoder.predict(encoded_imgs)
+
+# use Matplotlib
+import matplotlib.pyplot as plt
+
+n = 10  # how many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(x_test[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(decoded_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.show()
+
+##Adding a sparsity constraint on the encoded representation
+
+from keras import regularizers
+
+encoding_dim = 32
+
+input_img = Input(shape=(784,))
+# add a Dense layer with a L1 activity regularizer
+encoded = Dense(encoding_dim, activation='relu',
+                activity_regularizer=regularizers.l1(10e-5))(input_img)
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+autoencoder = Model(input_img, decoded)
