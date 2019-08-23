6d229ce9bde5033802aca29ad3047f37ee6d870d
DonatoMeoli <donato.meoli.95@gmail.com>
2019-08-23 15:21:03 +0200

    simplified condition in search.py

--- search.py
+++ search.py
@@ -738,8 +738,7 @@ class PeakFindingProblem(Problem):
         allowed_actions = []
         for action in self.defined_actions:
             next_state = vector_add(state, self.defined_actions[action])
-            if next_state[0] >= 0 and next_state[1] >= 0 and next_state[0] <= self.n - 1 and next_state[
-                1] <= self.m - 1:
+            if 0 <= next_state[0] <= self.n - 1 and 0 <= next_state[1] <= self.m - 1:
                 allowed_actions.append(action)
 
         return allowed_actions
--- tests/test_search.py
+++ tests/test_search.py
@@ -162,7 +162,6 @@ def test_recursive_best_first_search():
         index_goal = {0: [2, 2], 1: [0, 0], 2: [0, 1], 3: [0, 2], 4: [1, 0], 5: [1, 1], 6: [1, 2], 7: [2, 0], 8: [2, 1]}
         index_state = {}
         index = [[0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
-        x, y = 0, 0
 
         for i in range(len(state)):
             index_state[state[i]] = index[i]
