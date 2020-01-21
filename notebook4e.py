39895fd1665e28819e21e0f8ec9389c4ebbcde66
Tirth Patel <tirthasheshpatel@gmail.com>
2020-01-21 23:28:29 +0530

    FIXUP: Remove duplicated code

--- notebook4e.py
+++ notebook4e.py
@@ -18,7 +18,8 @@ from matplotlib.animation import FuncAnimation
 from games import TicTacToe, alpha_beta_player, random_player, Fig52Extended
 from learning import DataSet
 from logic import parse_definite_clause, standardize_variables, unify_mm, subst
-from search import GraphProblem, romania_map
+from search import GraphProblem, romania_map, Problem, Node
+from utils import PriorityQueue
 
 
 # ______________________________________________________________________________
@@ -1170,101 +1171,67 @@ def transpose(matrix): return list(zip(*matrix))
 
 ############################################################################################################
 
-
-class Problem(object):
-    """The abstract class for a formal problem. A new domain subclasses this,
-    overriding `actions` and `results`, and perhaps other methods.
-    The default heuristic is 0 and the default action cost is 1 for all states.
-    When yiou create an instance of a subclass, specify `initial`, and `goal` states 
-    (or give an `is_goal` method) and perhaps other keyword args for the subclass."""
-
-    def __init__(self, initial=None, goal=None, **kwds): 
-        self.__dict__.update(initial=initial, goal=goal, **kwds) 
-        
-    def actions(self, state):        raise NotImplementedError
-    def result(self, state, action): raise NotImplementedError
-    def is_goal(self, state):        return state == self.goal
-    def step_cost(self, s, a, s1): return 1
-    def h(self, node):               return 0
-    
-    def __str__(self):
-        return '{}({!r}, {!r})'.format(
-            type(self).__name__, self.initial, self.goal)
-
-
-class Node:
-    "A Node in a search tree."
-    def __init__(self, state, parent=None, action=None, path_cost=0):
-        self.__dict__.update(state=state, parent=parent, action=action, path_cost=path_cost)
-
-    def __repr__(self): return '<{}>'.format(self.state)
-    def __len__(self): return 0 if self.parent is None else (1 + len(self.parent))
-    def __lt__(self, other): return self.path_cost < other.path_cost
-
-
-failure = Node('failure', path_cost=np.inf) # Indicates an algorithm couldn't find a solution.
-cutoff  = Node('cutoff',  path_cost=np.inf) # Indicates iterative deepening search was cut off.
-
-def expand(problem, node):
-    "Expand a node, generating the children nodes."
-    s = node.state
-    for action in problem.actions(s):
-        s1 = problem.result(s, action)
-        cost = node.path_cost + problem.step_cost(s, action, s1)
-        yield Node(s1, node, action, cost)
-
-def path_actions(node):
-    "The sequence of actions to get to this node."
-    if node.parent is None:
-        return []
-    return path_actions(node.parent) + [node.action]
-
-def path_states(node):
-    "The sequence of states to get to this node."
-    if node in (cutoff, failure, None): 
-        return []
-    return path_states(node.parent) + [node.state]
-
 def straight_line_distance(A, B):
     "Straight-line distance between two points."
     return sum(abs(a - b)**2 for (a, b) in zip(A, B)) ** 0.5
 
+def random_lines(X=range(15, 130), Y=range(60), N=150, lengths=range(6, 12)):
+    """The set of cells in N random lines of the given lengths."""
+    result = set()
+    for _ in range(N):
+        x, y = random.choice(X), random.choice(Y)
+        dx, dy = random.choice(((0, 1), (1, 0)))
+        result |= line(x, y, dx, dy, random.choice(lengths))
+    return result
 
-class PriorityQueue:
-    """A queue in which the item with minimum f(item) is always popped first."""
-
-    def __init__(self, items=(), key=lambda x: x):
-        self.key = key
-        self.items = [] # a heap of (score, item) pairs
-        for item in items:
-            self.add(item)
-
-    def add(self, item):
-        """Add item to the queue."""
-        pair = (self.key(item), item)
-        heapq.heappush(self.items, pair)
-
-    def pop(self):
-        """Pop and return the item with min f(item) value."""
-        return heapq.heappop(self.items)[1]
-
-    def top(self): return self.items[0][1]
-
-    def __len__(self): return len(self.items)
+def line(x, y, dx, dy, length):
+    """A line of `length` cells starting at (x, y) and going in (dx, dy) direction."""
+    return {(x + i * dx, y + i * dy) for i in range(length)}
 
 
-class GridProblem(Problem):
-    """Finding a path on a 2D grid with obstacles. Obstacles are (x, y) cells."""
-
-    def __init__(self, initial=(0, 0), goal=(9, 9), obstacles=(), **kwds):
-        Problem.__init__(self, initial=initial, goal=goal,
-                         obstacles=set(obstacles) - {initial, goal}, **kwds)
-
+class AnimateProblem(Problem):
     directions = [(-1, -1), (0, -1), (1, -1),
                   (-1, 0),           (1,  0),
                   (-1, +1), (0, +1), (1, +1)]
-    
-    def step_cost(self, s, action, s1): return straight_line_distance(s, s1)
+
+    def __init__(self, solver, weight=1.4,
+                 height=20, width=40, cell_weights=None,
+                 initial=(1, 1), goal=(35, 19),
+                 obstacles=random_lines(X=range(40), Y=range(20), N=80, lengths=range(1, 7))):
+        """Animate the Grid Problem"""
+        self.height = height
+        self.width = width
+        self.initial = initial
+        self.goal = goal
+        self.obstacles = set(obstacles) - {self.initial, self.goal}
+        self.weight = weight
+        # We may change the cell_weights in case of Uniform Cost search
+        self.cell_weights = cell_weights
+        if self.cell_weights is None:
+            self.cell_weights = np.ones((self.width+5, self.height+5), dtype=np.int16)
+        # Define all the allowed solvers and their f-value function.
+        # TODO: Bidirectional Search, Iterative Deepening Search.
+        self.SOLVERS = {'astar': (lambda n: n.path_cost + self.h(n)),
+                        'wastar': (lambda n: n.path_cost + self.weight*self.h(n)),
+                        'bfs': (lambda n: n.depth),
+                        'dfs': (lambda n: -n.depth),
+                        'ucs': (lambda n: n.path_cost),
+                        'bestfs': (lambda n: self.h(n))
+                       }
+        self.solver_f = self.SOLVERS[solver] # Assign the solver's f-value function
+        self.solver = solver
+        self.__initial_node = Node(self.initial)
+        # Dictionary of reach nodes. Simlar to `explored` set.
+        self.reached = {self.initial: self.__initial_node}
+        # Frontier of nodes to be explored!
+        self.frontier = PriorityQueue(f=self.solver_f)
+        self.frontier.append(self.__initial_node)
+        # We will draw each frame onto this figure
+        self.fig, self.ax = plt.subplots(figsize=(10, 6))
+        self.solution = [(-1, -1)]
+        self.ax.axis('off')
+        self.ax.axis('equal')
+        self.done = False
     
     def h(self, node): return straight_line_distance(node.state, self.goal)
 
@@ -1282,65 +1249,10 @@ class GridProblem(Problem):
         """You can move one cell in any of `directions` to a non-obstacle cell."""
         x, y = state
         return {(x + dx, y + dy) for (dx, dy) in self.directions} - self.obstacles
-
-
-def random_lines(X=range(0, 10), Y=range(10), N=20, lengths=range(1, 3)):
-    """The set of cells in N random lines of the given lengths."""
-    result = set()
-    for _ in range(N):
-        x, y = random.choice(X), random.choice(Y)
-        dx, dy = random.choice(((0, 1), (1, 0)))
-        result |= line(x, y, dx, dy, random.choice(lengths))
-    return result
-
-
-def line(x, y, dx, dy, length):
-    """A line of `length` cells starting at (x, y) and going in (dx, dy) direction."""
-    return {(x + i * dx, y + i * dy) for i in range(length)}
-
-
-class AnimateProblem(GridProblem):
-    def __init__(self, solver, weight=1.4, cell_weights=None, **kwargs):
-        """Animate the Grid Problem"""
-        super().__init__(**kwargs)
-        self.weight = weight
-        # We may change the cell_weights in case of Uniform Cost search
-        self.cell_weights = cell_weights
-        if self.cell_weights is None:
-            self.cell_weights = np.ones((self.width+5, self.height+5), dtype=np.int16)
-        # Define all the allowed solvers and their f-value function.
-        # TODO: Bidirectional Search, Iterative Deepening Search.
-        self.SOLVERS = {'astar': (lambda n: n.path_cost + self.h(n)),
-                        'wastar': (lambda n: n.path_cost + self.weight*self.h(n)),
-                        'bfs': (lambda n: len(n)),
-                        'dfs': (lambda n: -len(n)),
-                        'ucs': (lambda n: n.path_cost),
-                        'bestfs': (lambda n: self.h(n))
-                       }
-        self.solver_f = self.SOLVERS[solver] # Assign the solver's f-value function
-        self.solver = solver
-        self.__initial_node = Node(self.initial)
-        # Dictionary of reach nodes. Simlar to `explored` set.
-        self.reached = {self.initial: self.__initial_node}
-        # Frontier of nodes to be explored!
-        self.frontier = PriorityQueue([self.__initial_node], key=self.solver_f)
-        # We will draw each frame onto this figure
-        self.fig, self.ax = plt.subplots(figsize=(10, 6))
-        self.solution = [(-1, -1)]
-        self.ax.axis('off')
-        self.ax.axis('equal')
-        self.done = False
     
-    def step_cost(self, s, a, s1):
+    def path_cost(self, c, state1, action, state2):
         """Return the cost of moving from s to s1"""
-        return self.cell_weights[s1[0]][s1[1]]
-    
-    def draw_walls(self):
-        """Draws wall around the grid to stop exploring redundant nodes"""
-        self.obstacles |= {(i, -2) for i in range(-2, self.width+4)}
-        self.obstacles |= {(i, self.height+4) for i in range(-2, self.width+4)}
-        self.obstacles |= {(-2, j) for j in range(-2, self.height+5)}
-        self.obstacles |= {(self.width+4, j) for j in range(-2, self.height+5)}
+        return c + self.cell_weights[state2[0]][state2[1]]
         
     def step(self, frame):
         """
@@ -1357,15 +1269,15 @@ class AnimateProblem(GridProblem):
         # Run the search algorithm for a single
         # node in the frontier.
         node = self.frontier.pop()
-        self.solution = path_states(node)
-        if self.is_goal(node.state):
+        self.solution = node.solution()
+        if self.goal_test(node.state):
             self.done = True
         else:
-            for child in expand(self, node):
+            for child in node.expand(self):
                 s = child.state
                 if s not in self.reached or child.path_cost < self.reached[s].path_cost:
                     self.reached[s] = child
-                    self.frontier.add(child)
+                    self.frontier.append(child)
 
         # Plot all the new states onto our figure
         # and return them to matplotlib for creating animation.
