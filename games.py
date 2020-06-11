5aeaf615d2e3d485cde72b4ad1f4050aee01d5ff
Sanders Lin <45224617+SandersLin@users.noreply.github.com>
2020-06-11 08:10:44 +0800

    games.py Gomoku (#1080)
    * update games.py connect 4 display method 
    
    original code displays board sideways. Fixed display method to print board bottom down
    
    * update games.py add Gomoku game
    
    Trivially addition of Gomoku, thanks to flexible implementation of TicTacToe class

--- games.py
+++ games.py
@@ -424,7 +424,13 @@ class ConnectFour(TicTacToe):
 
     def actions(self, state):
         return [(x, y) for (x, y) in state.moves
-                if y == 1 or (x, y - 1) in state.board]
+                if x == self.h or (x + 1 , y ) in state.board]
+
+class Gomoku(TicTacToe):
+    """Also known as Five in a row."""
+
+    def __init__(self, h=15, v=16, k=5):
+        TicTacToe.__init__(self, h, v, k)
 
 
 class Backgammon(StochasticGame):
