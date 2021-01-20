625f5b05b0f8f5d59e18129c4c5b998f151be4b4
RAHUL KUMAR <55033230+pctablet505@users.noreply.github.com>
2021-01-20 11:50:53 +0530

    Update agents4e.py

--- agents4e.py
+++ agents4e.py
@@ -34,16 +34,17 @@ EnvCanvas ## Canvas to display the environment of an EnvGUI
 # What if the grabber moves?
 # Speed control in GUI does not have any effect -- fix it.
 
-from utils4e import distance_squared, turn_heading
+import collections
+import copy
+import numbers
+import random
 from statistics import mean
-from ipythonblocks import BlockGrid
-from IPython.display import HTML, display, clear_output
 from time import sleep
 
-import random
-import copy
-import collections
-import numbers
+from IPython.display import HTML, display, clear_output
+from ipythonblocks import BlockGrid
+
+from utils4e import distance_squared, turn_heading
 
 
 # ______________________________________________________________________________
@@ -645,23 +646,6 @@ class GraphicEnvironment(XYEnvironment):
             result.append(row)
         return result
 
-    """
-    def run(self, steps=1000, delay=1):
-        "" "Run the Environment for given number of time steps,
-        but update the GUI too." ""
-        for step in range(steps):
-            sleep(delay)
-            if self.visible:
-                self.reveal()
-            if self.is_done():
-                if self.visible:
-                    self.reveal()
-                return
-            self.step()
-        if self.visible:
-            self.reveal()
-    """
-
     def run(self, steps=1000, delay=1):
         """Run the Environment for given number of time steps,
         but update the GUI too."""
@@ -679,7 +663,9 @@ class GraphicEnvironment(XYEnvironment):
     def reveal(self):
         """Display the BlockGrid for this world - the last thing to be added
         at a location defines the location color."""
+
         self.draw_world()
+
         # wait for the world to update and
         # apply changes to the same grid instead
         # of making a new one.
@@ -875,10 +861,10 @@ class WumpusEnvironment(XYEnvironment):
     def init_world(self, program):
         """Spawn items in the world based on probabilities from the book"""
 
-        "WALLS"
+        # WALLS
         self.add_walls()
 
-        "PITS"
+        # PITS
         for x in range(self.x_start, self.x_end):
             for y in range(self.y_start, self.y_end):
                 if random.random() < self.pit_probability:
@@ -888,7 +874,7 @@ class WumpusEnvironment(XYEnvironment):
                     self.add_thing(Breeze(), (x + 1, y), True)
                     self.add_thing(Breeze(), (x, y + 1), True)
 
-        "WUMPUS"
+        # WUMPUS
         w_x, w_y = self.random_location_inbounds(exclude=(1, 1))
         self.add_thing(Wumpus(lambda x: ""), (w_x, w_y), True)
         self.add_thing(Stench(), (w_x - 1, w_y), True)
@@ -896,10 +882,10 @@ class WumpusEnvironment(XYEnvironment):
         self.add_thing(Stench(), (w_x, w_y - 1), True)
         self.add_thing(Stench(), (w_x, w_y + 1), True)
 
-        "GOLD"
+        # GOLD
         self.add_thing(Gold(), self.random_location_inbounds(exclude=(1, 1)), True)
 
-        "AGENT"
+        # AGENT
         self.add_thing(Explorer(program), (1, 1), True)
 
     def get_world(self, show_walls=True):
