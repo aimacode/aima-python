389d5a72b2095ede4c87413ab75b3047701af7e5
Donato Meoli <donato.meoli.95@gmail.com>
2026-06-23 16:04:10 +0200

    Rename camelCase identifiers to snake_case (PEP 8)
    Make function and variable names consistent with the snake_case style
    used across the rest of the codebase:
    
    - planning.py: the *_graphPlan example helpers become *_graph_plan, plus
      graphPlan_solution, initialPlan, nConstraints, nPartial.
    - nlp.py: loadPageHTML, initPages, stripRawHTML, determineInlinks,
      findOutlinks, onlyWikipediaURLS, getInLinks, getOutLinks and their local
      variables (pagesIndex, pagesContent, inLinks, outLinks, etc.).
    - Updated all call sites in tests and notebooks, and snake_cased the
      affected test names and test-local variables.
    
    Class names (PascalCase) and the canvas strokeWidth method, which mirrors
    the JS canvas API, are intentionally left unchanged.

--- nlp.py
+++ nlp.py
@@ -380,7 +380,7 @@ def CYK_parse(words, grammar):
 # Page Ranking
 
 # First entry in list is the base URL, and then following are relative URL pages
-examplePagesSet = ["https://en.wikipedia.org/wiki/", "Aesthetics", "Analytic_philosophy",
+example_pages_set = ["https://en.wikipedia.org/wiki/", "Aesthetics", "Analytic_philosophy",
                    "Ancient_Greek", "Aristotle", "Astrology", "Atheism", "Baruch_Spinoza",
                    "Belief", "Betrand Russell", "Confucius", "Consciousness",
                    "Continental Philosophy", "Dialectic", "Eastern_Philosophy",
@@ -392,58 +392,58 @@ examplePagesSet = ["https://en.wikipedia.org/wiki/", "Aesthetics", "Analytic_phi
                    "Truth", "Western_philosophy"]
 
 
-def loadPageHTML(addressList):
+def load_page_html(address_list):
     """Download HTML page content for every URL address passed as argument"""
-    contentDict = {}
-    for addr in addressList:
+    content_dict = {}
+    for addr in address_list:
         with urllib.request.urlopen(addr) as response:
             raw_html = response.read().decode('utf-8')
             # Strip raw html of unnecessary content. Basically everything that isn't link or text
-            html = stripRawHTML(raw_html)
-            contentDict[addr] = html
-    return contentDict
+            html = strip_raw_html(raw_html)
+            content_dict[addr] = html
+    return content_dict
 
 
-def initPages(addressList):
+def init_pages(address_list):
     """Create a dictionary of pages from a list of URL addresses"""
     pages = {}
-    for addr in addressList:
+    for addr in address_list:
         pages[addr] = Page(addr)
     return pages
 
 
-def stripRawHTML(raw_html):
+def strip_raw_html(raw_html):
     """Remove the <head> section of the HTML which contains links to stylesheets etc.,
     and remove all other unnecessary HTML"""
     # TODO: Strip more out of the raw html
     return re.sub("<head>.*?</head>", "", raw_html, flags=re.DOTALL)  # remove <head> section
 
 
-def determineInlinks(page):
+def determine_inlinks(page):
     """Given a set of pages that have their outlinks determined, we can fill
     out a page's inlinks by looking through all other page's outlinks"""
     inlinks = []
-    for addr, indexPage in pagesIndex.items():
-        if page.address == indexPage.address:
+    for addr, index_page in pages_index.items():
+        if page.address == index_page.address:
             continue
-        elif page.address in indexPage.outlinks:
+        elif page.address in index_page.outlinks:
             inlinks.append(addr)
     return inlinks
 
 
-def findOutlinks(page, handleURLs=None):
+def find_outlinks(page, handle_urls=None):
     """Search a page's HTML content for URL links to other pages"""
-    urls = re.findall(r'href=[\'"]?([^\'" >]+)', pagesContent[page.address])
-    if handleURLs:
-        urls = handleURLs(urls)
+    urls = re.findall(r'href=[\'"]?([^\'" >]+)', pages_content[page.address])
+    if handle_urls:
+        urls = handle_urls(urls)
     return urls
 
 
-def onlyWikipediaURLS(urls):
+def only_wikipedia_urls(urls):
     """Some example HTML page data is from wikipedia. This function converts
     relative wikipedia links to full wikipedia URLs"""
-    wikiURLs = [url for url in urls if url.startswith('/wiki/')]
-    return ["https://en.wikipedia.org" + url for url in wikiURLs]
+    wiki_urls = [url for url in urls if url.startswith('/wiki/')]
+    return ["https://en.wikipedia.org" + url for url in wiki_urls]
 
 
 # ______________________________________________________________________________
@@ -458,25 +458,25 @@ def expand_pages(pages):
             expanded[addr] = page
         for inlink in page.inlinks:
             if inlink not in expanded:
-                expanded[inlink] = pagesIndex[inlink]
+                expanded[inlink] = pages_index[inlink]
         for outlink in page.outlinks:
             if outlink not in expanded:
-                expanded[outlink] = pagesIndex[outlink]
+                expanded[outlink] = pages_index[outlink]
     return expanded
 
 
 def relevant_pages(query):
     """Relevant pages are pages that contain all of the query words. They are obtained by
     intersecting the hit lists of the query words."""
-    hit_intersection = {addr for addr in pagesIndex}
+    hit_intersection = {addr for addr in pages_index}
     query_words = query.split()
     for query_word in query_words:
         hit_list = set()
-        for addr in pagesIndex:
-            if query_word.lower() in pagesContent[addr].lower():
+        for addr in pages_index:
+            if query_word.lower() in pages_content[addr].lower():
                 hit_list.add(addr)
         hit_intersection = hit_intersection.intersection(hit_list)
-    return {addr: pagesIndex[addr] for addr in hit_intersection}
+    return {addr: pages_index[addr] for addr in hit_intersection}
 
 
 def normalize(pages):
@@ -503,16 +503,16 @@ class ConvergenceDetector(object):
         return self.detect()
 
     def detect(self):
-        curr_hubs = [page.hub for addr, page in pagesIndex.items()]
-        curr_auths = [page.authority for addr, page in pagesIndex.items()]
+        curr_hubs = [page.hub for addr, page in pages_index.items()]
+        curr_auths = [page.authority for addr, page in pages_index.items()]
         if self.hub_history is None:
             self.hub_history, self.auth_history = [], []
         else:
-            diffsHub = [abs(x - y) for x, y in zip(curr_hubs, self.hub_history[-1])]
-            diffsAuth = [abs(x - y) for x, y in zip(curr_auths, self.auth_history[-1])]
-            aveDeltaHub = sum(diffsHub) / float(len(pagesIndex))
-            aveDeltaAuth = sum(diffsAuth) / float(len(pagesIndex))
-            if aveDeltaHub < 0.01 and aveDeltaAuth < 0.01:  # may need tweaking
+            diffs_hub = [abs(x - y) for x, y in zip(curr_hubs, self.hub_history[-1])]
+            diffs_auth = [abs(x - y) for x, y in zip(curr_auths, self.auth_history[-1])]
+            ave_delta_hub = sum(diffs_hub) / float(len(pages_index))
+            ave_delta_auth = sum(diffs_auth) / float(len(pages_index))
+            if ave_delta_hub < 0.01 and ave_delta_auth < 0.01:  # may need tweaking
                 return True
         if len(self.hub_history) > 2:  # prevent list from getting long
             del self.hub_history[0]
@@ -522,32 +522,32 @@ class ConvergenceDetector(object):
         return False
 
 
-def getInLinks(page):
+def get_in_links(page):
     if not page.inlinks:
-        page.inlinks = determineInlinks(page)
-    return [addr for addr, p in pagesIndex.items() if addr in page.inlinks]
+        page.inlinks = determine_inlinks(page)
+    return [addr for addr, p in pages_index.items() if addr in page.inlinks]
 
 
-def getOutLinks(page):
+def get_out_links(page):
     if not page.outlinks:
-        page.outlinks = findOutlinks(page)
-    return [addr for addr, p in pagesIndex.items() if addr in page.outlinks]
+        page.outlinks = find_outlinks(page)
+    return [addr for addr, p in pages_index.items() if addr in page.outlinks]
 
 
 # ______________________________________________________________________________
 # HITS Algorithm
 
 class Page(object):
-    def __init__(self, address, inLinks=None, outLinks=None, hub=0, authority=0):
+    def __init__(self, address, in_links=None, out_links=None, hub=0, authority=0):
         self.address = address
         self.hub = hub
         self.authority = authority
-        self.inlinks = inLinks
-        self.outlinks = outLinks
+        self.inlinks = in_links
+        self.outlinks = out_links
 
 
-pagesContent = {}  # maps Page relative or absolute URL/location to page's HTML content
-pagesIndex = {}
+pages_content = {}  # maps Page relative or absolute URL/location to page's HTML content
+pages_index = {}
 convergence = ConvergenceDetector()  # assign function to variable to mimic pseudocode's syntax
 
 
@@ -562,8 +562,8 @@ def HITS(query):
         hub = {p: pages[p].hub for p in pages}
         for p in pages:
             # p.authority ← ∑i Inlinki(p).Hub
-            pages[p].authority = sum(hub[x] for x in getInLinks(pages[p]))
+            pages[p].authority = sum(hub[x] for x in get_in_links(pages[p]))
             # p.hub ← ∑i Outlinki(p).Authority
-            pages[p].hub = sum(authority[x] for x in getOutLinks(pages[p]))
+            pages[p].hub = sum(authority[x] for x in get_out_links(pages[p]))
         normalize(pages)
     return pages
